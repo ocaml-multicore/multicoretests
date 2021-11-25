a45e279ac2509d2aa0641783ac69ad6a045e79f7
Jan Midtgaard <jan@tarides.com>
2021-11-25 13:01:10 +0100

    add STM examples

---
+++ src/conclist_test.ml
@@ -0,0 +1,102 @@
+open QCheck
+
+(** a simple concurrent list - from Sadiq *)
+module CList =
+struct
+  type conc_list = { value: int; next: conc_list option }
+
+  let rec add_node list_head n =
+    (* try to add a new node to head *)
+    let old_head = Atomic.get list_head in
+    let new_node = { value = n ; next = (Some old_head) } in
+    (* introduce bug *)
+    if Atomic.get list_head = old_head then begin
+        Atomic.set list_head new_node;
+        true
+      end
+    else
+      add_node list_head n
+
+  let list_init i = Atomic.make { value = i ; next = None }
+
+  let member list_head n =
+    let rec check_from_node node =
+      match (node.value, node.next) with
+      | (v, _) when v = n -> true
+      | (_, None) -> false
+      | (_ , Some(next_node)) ->
+         check_from_node next_node
+    in
+    (* try to find the node *)
+    check_from_node (Atomic.get list_head)
+
+  let add_and_check list_head n () =
+    assert(add_node list_head n);
+    assert(member list_head n)
+end
+
+
+(** This is a parallel test of the buggy concurrent list CList *)
+
+module CLConf =
+struct
+  type cmd = Add_node of int | Member of int [@@deriving show { with_path = false }]
+  type state = int list
+  type sut = CList.conc_list Atomic.t
+
+  let arb_cmd s =
+    let int_gen = Gen.nat in
+    let mem_gen =
+      if s=[]
+      then int_gen
+      else Gen.oneof [int_gen; Gen.oneofl s]
+    in
+    QCheck.make ~print:show_cmd
+      (Gen.oneof
+         [ Gen.map (fun i -> Add_node i) int_gen;
+	   Gen.map (fun i -> Member i) mem_gen; ])
+
+  let init_state  = [0]
+  let init_sut () = CList.list_init 0
+  let cleanup _   = ()
+
+  let next_state c s = match c with
+    | Add_node i -> i::s
+    | Member _i  -> s
+
+  type res = RAdd_node of bool | RMember of bool [@@deriving show { with_path = false }]
+
+  let run c r = match c with
+    | Add_node i -> RAdd_node (CList.add_node r i)
+    | Member i   -> RMember (CList.member r i)
+
+  let precond _ _ = true
+
+  let postcond c s res = match c,res with
+    | Add_node _, RAdd_node v -> v = true
+    | Member i,   RMember v   -> v = List.mem i s
+    | _,_ -> false
+end
+
+module CLT = STM.Make(CLConf)
+
+let agree_test_par ~count ~name =
+  let seq_len = 20 in
+  let par_len = 15 in
+  Non_det.Test.make ~count ~name
+    (CLT.arb_cmds_par seq_len par_len) CLT.agree_prop_par
+
+let agree_test_pardomlib ~count ~name =
+  let seq_len = 20 in
+  let par_len = 15 in
+  Non_det.Test.make ~count ~name
+    (CLT.arb_cmds_par seq_len par_len) CLT.agree_prop_pardomlib
+
+;;
+Non_det.QCheck_runner.run_tests ~verbose:true [
+    CLT.agree_test           ~count:1000 ~name:"sequential test of CList";
+    CLT.agree_test_par       ~count:1000 ~name:"parallel test of CList (w/repeat)";
+        agree_test_par       ~count:1000 ~name:"parallel test of CList (w/non_det module)";
+    CLT.agree_test_pardomlib ~count:1000 ~name:"parallel test of CList (w/Domainslib.Task and repeat)";
+        agree_test_pardomlib ~count:1000 ~name:"parallel test of CList (w/Domainslib.Task and non_det module)";
+  ]
---
+++ src/ref_test.ml
@@ -0,0 +1,118 @@
+open QCheck
+
+(** This is a parallel test of refs *)
+
+module Sut =
+  struct
+    let sut = ref 0
+    let get () = !sut
+    let set i  = sut:=i
+    let add i  = let old = !sut in sut:=i + old (* buggy: not atomic *)
+    let incr () = incr sut     (* buggy: not guaranteed to be atomic *)
+    let decr () = decr sut     (* buggy: not guaranteed to be atomic *)
+end
+
+module RConf =
+struct
+  type cmd =
+    | Get
+    | Set of int
+    | Add of int
+    | Incr
+    | Decr [@@deriving show { with_path = false }]
+  type state = int
+  type sut = int ref
+
+  let arb_cmd _s =
+    let int_gen = Gen.nat in
+    QCheck.make ~print:show_cmd
+      (Gen.oneof
+         [Gen.return Get;
+	  Gen.map (fun i -> Set i) int_gen;
+	  Gen.map (fun i -> Add i) int_gen;
+          Gen.return Incr;
+	  Gen.return Decr;
+         ])
+
+  let init_state  = 0
+  let init_sut () = Sut.sut
+  let cleanup _   = Sut.set 0
+
+  let next_state c s = match c with
+    | Get   -> s
+    | Set i -> i (*if i<>1213 then i else s*) (* an artificial fault *)
+    | Add i -> s+i
+    | Incr  -> s+1
+    | Decr  -> s-1
+
+  let precond _ _ = true
+
+  type res = RGet of int | RSet | RAdd | RIncr | RDecr [@@deriving show { with_path = false }]
+
+  let run c _r =
+    match c with
+    | Get   -> RGet (Sut.get ())
+    | Set i -> (Sut.set i; RSet)
+    | Add i -> (Sut.add i; RAdd)
+    | Incr  -> (Sut.incr (); RIncr)
+    | Decr  -> (Sut.decr (); RDecr)
+
+  let postcond c s res = match c,res with
+    | Get, RGet v -> v = s (*&& v<>42*) (*an injected bug*)
+    | Set _, RSet -> true
+    | Add _, RAdd -> true
+    | Incr, RIncr -> true
+    | Decr, RDecr -> true
+    | _,_ -> false
+end
+
+
+module RT = STM.Make(RConf)
+
+let agree_test_par ~count ~name =
+  let seq_len = 20 in
+  let par_len = 15 in
+  Non_det.Test.make ~count ~name
+    (RT.arb_cmds_par seq_len par_len) RT.agree_prop_par
+
+let agree_test_pardomlib ~count ~name =
+  let seq_len = 20 in
+  let par_len = 15 in
+  Non_det.Test.make ~count ~name
+    (RT.arb_cmds_par seq_len par_len) RT.agree_prop_pardomlib
+
+
+module RConfGC = STM.AddGC(RConf)
+module RTGC = STM.Make(RConfGC)
+
+let agree_prop_pargc =
+  (fun (seq_pref,cmds1,cmds2) ->
+    assume (RTGC.cmds_ok RConfGC.init_state (seq_pref@cmds1));
+    assume (RTGC.cmds_ok RConfGC.init_state (seq_pref@cmds2));
+    let sut = RConfGC.init_sut () in
+    let res = RTGC.interp_sut_res sut seq_pref in
+    let dom1 = Domain.spawn (fun () -> (*Gc.minor(); Gc.minor();*) RTGC.interp_sut_res sut cmds1) in
+    let dom2 = Domain.spawn (fun () -> RTGC.interp_sut_res sut cmds2) in
+    let obs1 = Domain.join dom1 in
+    let obs2 = Domain.join dom2 in
+    let res  = RTGC.check_obs res obs1 obs2 RConfGC.init_state in
+    let ()   = RConf.cleanup sut in
+    res)
+
+let agree_test_pargc ~count ~name =
+  let rep_count = 50 in
+  let seq_len = 20 in
+  let par_len = 15 in
+  Test.make ~count ~name
+    (RTGC.arb_cmds_par seq_len par_len)
+    (STM.repeat rep_count agree_prop_pargc)
+
+;;
+Non_det.QCheck_runner.run_tests ~verbose:true [
+    RT.agree_test           ~count:1000 ~name:"sequential test of global ref";
+    RT.agree_test_par       ~count:1000 ~name:"parallel test of global ref (w/repeat)";
+       agree_test_par       ~count:1000 ~name:"parallel test of global ref (w/non_det module)";
+    RT.agree_test_pardomlib ~count:1000 ~name:"parallel test of global ref (w/Domainslib.Task and repeat)";
+       agree_test_pardomlib ~count:1000 ~name:"parallel test of global ref (w/Domainslib.Task and non_det module)";
+       agree_test_pargc     ~count:1000 ~name:"parallel test of global ref (w/repeat and AddGC functor)";
+  ]
