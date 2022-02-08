aff1e39e6f93cef9871b8845c6d763f5235e1ef1
Jan Midtgaard <mail@janmidtgaard.dk>
2022-02-08 15:16:36 +0100

    add another atomic test

--- src/lin_tests.ml
+++ src/lin_tests.ml
@@ -109,6 +109,47 @@ end
 
 module AT = Lin.Make(AConf)
 
+(** A variant of the above with 3 Atomics *)
+module A3Conf =
+struct
+  type t = int Atomic.t array
+
+  type cmd =
+    | Get of var
+    | Set of var * int'
+    | Exchange of var * int'
+    | Compare_and_set of var * int' * int'
+    | Fetch_and_add of var * int'
+    | Incr of var
+    | Decr of var [@@deriving qcheck, show { with_path = false }]
+  and int' = int [@gen Gen.nat]
+  and var  = int [@gen Gen.int_bound 2]
+
+  type res =
+    | RGet of int
+    | RSet
+    | RExchange of int
+    | RFetch_and_add of int
+    | RCompare_and_set of bool
+    | RIncr
+    | RDecr [@@deriving show { with_path = false }]
+
+  let init () = [| Atomic.make 0; Atomic.make 0; Atomic.make 0 |]
+
+  let run c env = match c with
+    | Get v                    -> RGet (Atomic.get env.(v))
+    | Set (v,i)                -> (Atomic.set env.(v) i; RSet)
+    | Exchange (v,i)           -> RExchange (Atomic.exchange env.(v) i)
+    | Fetch_and_add (v,i)      -> RFetch_and_add (Atomic.fetch_and_add env.(v) i)
+    | Compare_and_set (v,seen,nval) -> RCompare_and_set (Atomic.compare_and_set env.(v) seen nval)
+    | Incr v                   -> (Atomic.incr env.(v); RIncr)
+    | Decr v                   -> (Atomic.decr env.(v); RDecr)
+
+  let cleanup _ = ()
+end
+
+module A3T = Lin.Make(A3Conf)
+
 
 (** ********************************************************************** *)
 (**                      Tests of thread-unsafe [Hashtbl]                  *)
@@ -167,5 +208,6 @@ QCheck_runner.run_tests_main [
   RT.lin_test     ~count:1000 ~name:"ref test";
   CLT.lin_test    ~count:1000 ~name:"CList test";
   AT.lin_test     ~count:1000 ~name:"Atomic test";
+  A3T.lin_test    ~count:1000 ~name:"Atomic3 test";
   HT.lin_test     ~count:1000 ~name:"Hashtbl test";
 ]
