35693910509eb95e9e196a83d1d671f53cbdefc3
Olivier Nicole <olivier@chnik.fr>
2022-04-14 12:19:45 +0200

    Add API-describing DSL for automated linearizability testing

---
+++ test/lin_api.ml
@@ -0,0 +1,21 @@
+module Atomic_spec : Lin_api.ApiSpec = struct
+  open Lin_api (* FIXME add Gen.nat *)
+  type t = int Atomic.t
+  let init () = Atomic.make 0
+  let cleanup _ = ()
+  let api =
+    [ val_ "Atomic.get"             Atomic.get             (t @-> returning int);
+      val_ "Atomic.set"             Atomic.set             (t @-> int @-> returning unit);
+      val_ "Atomic.exchange"        Atomic.exchange        (t @-> int @-> returning int);
+      val_ "Atomic.fetch_and_add"   Atomic.fetch_and_add   (t @-> int @-> returning int);
+      val_ "Atomic.compare_and_set" Atomic.compare_and_set (t @-> int @-> int @-> returning bool);
+      val_ "Atomic.incr"            Atomic.incr            (t @-> returning unit);
+      val_ "Atomic.decr"            Atomic.decr            (t @-> returning unit) ]
+end
+
+module Lin_atomic = Lin_api.Make (Atomic_spec)
+
+let () =
+  QCheck_runner.run_tests_main
+    [ Lin_atomic.lin_test      ~count:100  ~name:"Atomic"      `Domain;
+    ]
