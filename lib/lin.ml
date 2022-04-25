62bad5ff0764d5b315a29690d691f399f395ca06
Jan Midtgaard <mail@janmidtgaard.dk>
2022-04-26 00:09:24 +0200

    adjust par cmd list length

--- lib/lin.ml
+++ lib/lin.ml
@@ -261,7 +261,7 @@ module Make(Spec : CmdSpec)
 
   (* Linearizability test based on [Domain], [Thread], or [Effect] *)
   let lin_test ~count ~name (lib : [ `Domain | `Thread | `Effect ]) =
-    let seq_len,par_len = 20,15 in
+    let seq_len,par_len = 20,12 in
     match lib with
     | `Domain ->
         let arb_cmd_triple = arb_cmds_par seq_len par_len in
@@ -270,8 +270,8 @@ module Make(Spec : CmdSpec)
           arb_cmd_triple (repeat rep_count lin_prop_domain)
     | `Thread ->
         let arb_cmd_triple = arb_cmds_par seq_len par_len in
-        let rep_count = 100 in
-        Test.make ~count ~retries:10 ~name:("Linearizable " ^ name ^ " with Thread")
+        let rep_count = 125 in
+        Test.make ~count ~retries:5 ~name:("Linearizable " ^ name ^ " with Thread")
           arb_cmd_triple (repeat rep_count lin_prop_thread)
     | `Effect ->
         (* this generator is over [EffSpec.cmd] including [SchedYield], not [Spec.cmd] like the above two *)
