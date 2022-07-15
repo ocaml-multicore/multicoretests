f241cfb56f875d9ef27b0e0f412e8f5c190e7e85
Jan Midtgaard <mail@janmidtgaard.dk>
2022-07-15 13:58:49 +0200

    protect Buffer test some more against 'usual' exceptions

--- src/buffer/stm_tests.ml
+++ src/buffer/stm_tests.ml
@@ -26,9 +26,13 @@ struct
   type state = char list (* in reverse *)
   type sut = Buffer.t
 
+  let shrink_cmd c = match c with
+    | Add_string s -> Iter.map (fun s -> Add_string s) (Shrink.string s)
+    | _ -> Iter.empty
+
   let arb_cmd s =
     let int_gen,string_gen = Gen.(small_nat,small_string) in
-    QCheck.make ~print:show_cmd
+    QCheck.make ~print:show_cmd (*~shrink:shrink_cmd*)
       Gen.(oneof [return Contents;
                   return To_bytes;
                   map2 (fun off len -> Sub (off, len)) int_gen int_gen;
@@ -84,21 +88,22 @@ struct
     | _ -> true
 
   let run c b = match c with
-    | Contents        -> Res (string, Buffer.contents b)
-    | To_bytes        -> Res (bytes,  Buffer.to_bytes b)
+    | Contents        -> Res (result string exn, protect Buffer.contents b) (* protect from Invalid_argument("String.sub / Bytes.sub") *)
+    | To_bytes        -> Res (result bytes exn, protect Buffer.to_bytes b) (* protect from Invalid_argument("String.sub / Bytes.sub") *)
     | Sub (off, len)  -> Res (result string exn, protect (Buffer.sub b off) len)
     | Nth i           -> Res (result char exn, protect (Buffer.nth b) i)
     | Length          -> Res (int, Buffer.length b)
-    | Clear           -> Res (unit, Buffer.clear b)
-    | Reset           -> Res (unit, Buffer.reset b)
-    | Add_char ch     -> Res (unit, Buffer.add_char b ch)
-    | Add_string str  -> Res (unit, Buffer.add_string b str)
-    | Add_bytes bytes -> Res (unit, Buffer.add_bytes b bytes)
+    | Clear           -> Res (result unit exn, protect Buffer.clear b) (* protect from Invalid_argument("String.sub / Bytes.sub") *)
+    | Reset           -> Res (result unit exn, protect Buffer.reset b) (* protect from Invalid_argument("String.sub / Bytes.sub") *)
+    | Add_char ch     -> Res (result unit exn, protect (Buffer.add_char b) ch)     (* protect from assertion failure *)
+    | Add_string str  -> Res (result unit exn, protect (Buffer.add_string b) str)  (* protect from assertion failure *)
+    | Add_bytes bytes -> Res (result unit exn, protect (Buffer.add_bytes b) bytes) (* protect from assertion failure *)
     | Truncate i      -> Res (result unit exn, protect (Buffer.truncate b) i)
 
   let postcond c s res = match c, res with
-    | Contents, Res ((String,_),str)   -> explode str = List.rev s
-    | To_bytes, Res ((Bytes,_), bytes) -> bytes = Bytes.of_string (to_string s)
+    | Contents, Res ((Result (String,Exn),_),Ok str) -> explode str = List.rev s
+  (*| Contents, Res ((String,_),str)   -> explode str = List.rev s*)
+    | To_bytes, Res ((Result (Bytes,Exn),_), r) -> r = Ok (Bytes.of_string (to_string s))
     | Sub (off, len), Res ((Result (String,Exn),_), str) ->
        if off > List.length s || off + len > List.length s
        then str = Error (Invalid_argument "Buffer.sub")
@@ -108,11 +113,11 @@ struct
        then r = Error (Invalid_argument "Buffer.nth")
        else r = Ok (List.nth (List.rev s) i)
     | Length, Res ((Int,_),i) -> i = List.length s
-    | Clear, Res ((Unit,_),_) -> true
-    | Reset, Res ((Unit,_),_) -> true
-    | Add_char _, Res ((Unit,_),_)   -> true
-    | Add_string _, Res ((Unit,_),_) -> true
-    | Add_bytes _, Res ((Unit,_),_)  -> true
+    | Clear, Res ((Result (Unit,Exn),_),r) -> r = Ok ()
+    | Reset, Res ((Result (Unit,Exn),_),r) -> r = Ok ()
+    | Add_char _, Res ((Result (Unit,Exn),_),r) -> r = Ok ()
+    | Add_string _, Res ((Result (Unit,Exn),_),r) -> r = Ok ()
+    | Add_bytes _, Res ((Result (Unit,Exn),_),r) -> r = Ok ()
     | Truncate i, Res ((Result (Unit,Exn),_),r) ->
        if i < 0 || i > List.length s
        then r = Error (Invalid_argument "Buffer.truncate")
@@ -126,6 +131,6 @@ module BufferSTM = STM.Make(BConf)
 Util.set_ci_printing ()
 ;;
 QCheck_runner.run_tests_main
-  (let count = 100 in
+  (let count = 1000 in
    [BufferSTM.agree_test         ~count ~name:"buffer test";         (* this test is expected to succeed *)
     BufferSTM.neg_agree_test_par ~count ~name:"buffer test parallel" (* this test is expected to fail *)])
