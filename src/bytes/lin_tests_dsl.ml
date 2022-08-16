4df773ddd4663ef60453a12cd8e8f7645fc62d71
Jan Midtgaard <mail@janmidtgaard.dk>
2022-08-16 12:38:30 +0200

    update Bytes tests to multiple ts

--- src/bytes/lin_tests_dsl.ml
+++ src/bytes/lin_tests_dsl.ml
@@ -3,16 +3,28 @@
 (* ********************************************************************** *)
 module BConf = struct
   type t = Bytes.t
-  let init () = Stdlib.Bytes.make 42 '0'
+  let init () = Bytes.make 8 '0'
   let cleanup _ = ()
 
   open Lin_api
+  let int = nat_small
+(*let int = int_bound 10*)
   let api = [
-    val_ "Bytes.get"         Bytes.get         (t @-> int @-> returning_or_exc char);
-    val_ "Bytes.sub_string"  Bytes.sub_string  (t @-> int @-> int @-> returning_or_exc string);
     val_ "Bytes.length"      Bytes.length      (t @-> returning int);
+    val_ "Bytes.get"         Bytes.get         (t @-> int @-> returning_or_exc char);
+    val_ "Bytes.set"         Bytes.set         (t @-> int @-> char @-> returning_or_exc unit);
+ (* val_ "Bytes.create"      Bytes.create      (int @-> returning_or_exc_ t); *) (* result contains arbitrary bytes, hence non-det. output! *)
+    val_ "Bytes.make"        Bytes.make        (int @-> char @-> returning_or_exc_ t);
+ (* val_ "Bytes.empty"       Bytes.empty       (returning_ t); *)
+    val_ "Bytes.copy"        Bytes.copy        (t @-> returning_ t);
+    val_ "Bytes.of_string"   Bytes.of_string   (string @-> returning_ t);
+    val_ "Bytes.to_string"   Bytes.to_string   (t @-> returning string);
+    val_ "Bytes.sub_string"  Bytes.sub_string  (t @-> int @-> int @-> returning_or_exc string);
     val_ "Bytes.fill"        Bytes.fill        (t @-> int @-> int @-> char @-> returning_or_exc unit);
     val_ "Bytes.blit_string" Bytes.blit_string (string @-> int @-> t @-> int @-> int @-> returning_or_exc unit);
+    val_ "Bytes.trim"        Bytes.trim        (t @-> returning_ t);
+    val_ "Bytes.escaped"     Bytes.escaped     (t @-> returning_ t);
+    val_ "Bytes.index"       Bytes.index       (t @-> char @-> returning_or_exc int);
     val_ "Bytes.index_from"  Bytes.index_from  (t @-> int @-> char @-> returning_or_exc int)]
 end
 
