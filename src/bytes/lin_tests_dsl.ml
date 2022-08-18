df6b489492acc51165d8c507ca5b41deabc7bcff
Jan Midtgaard <mail@janmidtgaard.dk>
2022-08-18 08:54:52 +0200

    make Bytes test a negative test

--- src/bytes/lin_tests_dsl.ml
+++ src/bytes/lin_tests_dsl.ml
@@ -33,6 +33,6 @@ module BT = Lin_api.Make(BConf)
 Util.set_ci_printing ()
 ;;
 QCheck_base_runner.run_tests_main [
-  BT.lin_test `Domain ~count:1000 ~name:"Lin_api Bytes test with Domain";
-  BT.lin_test `Thread ~count:1000 ~name:"Lin_api Bytes test with Thread";
+  BT.neg_lin_test `Domain ~count:1000 ~name:"Lin_api Bytes test with Domain";
+  BT.lin_test     `Thread ~count:1000 ~name:"Lin_api Bytes test with Thread";
 ]
