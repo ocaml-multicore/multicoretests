1ff9b6d54bee767817fb56479127eb1229d25eac
Jan Midtgaard <mail@janmidtgaard.dk>
2022-08-19 10:58:06 +0200

    update Hashtbl deps

--- src/hashtbl/stm_tests.ml
+++ src/hashtbl/stm_tests.ml
@@ -121,7 +121,7 @@ module HTest = STM.Make(HConf)
 ;;
 Util.set_ci_printing ()
 ;;
-QCheck_runner.run_tests_main
+QCheck_base_runner.run_tests_main
   (let count = 200 in
    [HTest.agree_test         ~count ~name:"STM Hashtbl test sequential";
     HTest.neg_agree_test_par ~count ~name:"STM Hashtbl test parallel";
