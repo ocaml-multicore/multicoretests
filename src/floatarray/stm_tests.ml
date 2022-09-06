402d32ae68a062e9a561938bfcbd1c487c226430
Lucccyo <cha.git@mailo.fr>
2022-09-06 18:25:15 +0200

    add STM tests for floatarray module

---
+++ src/floatarray/stm_tests.ml
@@ -0,0 +1,131 @@
+open QCheck
+open STM
+open Util
+
+(** parallel STM tests of Float.Array *)
+
+module FAConf =
+struct
+  type cmd =
+    | Length
+    | Get of int
+    | Set of int * float
+
+    (* STM don't support floatarray type for the moment*)
+    (* | Sub of int * int
+    | Copy *)
+
+    | Fill of int * int * float
+    | To_list
+    | Mem of float
+    | Sort
+    | To_seq
+  [@@deriving show { with_path = false }]
+
+  type state = float list
+  type sut = Float.Array.t
+
+  let arb_cmd s =
+    let int_gen = Gen.(oneof [small_nat; int_bound (List.length s - 1)]) in
+    let float_gen = Gen.float in
+    QCheck.make ~print:show_cmd (*~shrink:shrink_cmd*)
+      Gen.(oneof
+             [ return Length;
+               map (fun i -> Get i) int_gen;
+               map2 (fun i f -> Set (i,f)) int_gen float_gen;
+
+               (* STM don't support floatarray type for the moment*)
+               (* map2 (fun i len -> Sub (i,len)) int_gen int_gen; (* hack: reusing int_gen for length *)
+               return Copy; *)
+
+               map3 (fun i len c -> Fill (i,len,c)) int_gen int_gen float_gen; (* hack: reusing int_gen for length *)
+               return To_list;
+               map (fun f -> Mem f) float_gen;
+               return Sort;
+               return To_seq;
+             ])
+
+  let floatarray_size = 16
+
+  let init_state  = List.init floatarray_size (fun _ -> 1.0)
+
+  let next_state f s = match f with
+    | Length -> s
+    | Get _  -> s
+    | Set (i,f) ->
+      List.mapi (fun j f' -> if i=j then f else f') s
+
+    (* STM don't support floatarray type for the moment*)
+    (* | Sub (_,_) -> s
+    | Copy -> s *)
+
+    | Fill (i,l,f) ->
+      if i >= 0 && l >= 0 && i+l-1 < List.length s
+      then
+        List.mapi (fun j f' -> if i <= j && j <= i+l-1 then f else f') s
+      else s
+    | To_list -> s
+    | Mem _ -> s
+    | Sort -> List.sort Float.compare s
+    | To_seq -> s
+
+  let init_sut () = Float.Array.make floatarray_size 1.0
+  let cleanup _   = ()
+
+  let precond f _s = match f with
+    | _ -> true
+
+  let run f fa = match f with
+    | Length       -> Res (int, Float.Array.length fa)
+    | Get i        -> Res (result float exn, protect (Float.Array.get fa) i)
+    | Set (i,f)    -> Res (result unit exn, protect (Float.Array.set fa i) f)
+
+    (* STM don't support floatarray type for the moment*)
+    (* | Sub (i,l)    -> Res (result (floatarray) exn, protect (Float.Array.sub fa i) l) *)
+    (* | Copy         -> Res (floatarray, Float.Array.copy fa) *)
+
+    | Fill (i,l,f) -> Res (result unit exn, protect (Float.Array.fill fa i l) f)
+    | To_list      -> Res (list float, Float.Array.to_list fa)
+    | Mem f        -> Res (bool, Float.Array.mem f fa)
+    | Sort         -> Res (unit, Float.Array.sort Float.compare fa)
+    | To_seq       -> Res (seq float, List.to_seq (List.of_seq (Float.Array.to_seq fa)))
+
+  let postcond f (s:float list) res = match f, res with
+    | Length, Res ((Int,_),i) -> i = List.length s
+    | Get i, Res ((Result (Float,Exn),_), r) ->
+      if i < 0 || i >= List.length s
+      then r = Error (Invalid_argument "index out of bounds")
+      else r = Ok (List.nth s i)
+    | Set (i,_), Res ((Result (Unit,Exn),_), r) ->
+      if i < 0 || i >= List.length s
+      then r = Error (Invalid_argument "index out of bounds")
+      else r = Ok ()
+
+    (* STM don't support floatarray type for the moment*)
+    (* | Sub (i,l), Res ((Result (Float.Array Float,Exn),_), r) ->
+      if i < 0 || l < 0 || i+l > List.length s
+      then r = Error (Invalid_argument "Float.Array.sub")
+      else r = Ok (Float.Array.of_list (List.filteri (fun j _ -> i <= j && j <= i+l-1) s)) *)
+    (* | Copy, Res ((Float.Array Float,_),r) -> Float.Array.to_list r = s *)
+
+    | Fill (i,l,_), Res ((Result (Unit,Exn),_), r) ->
+      if i < 0 || l < 0 || i+l > List.length s
+      then r = Error (Invalid_argument "Float.Array.fill")
+      else r = Ok ()
+    | To_list, Res ((List Float,_),fs) -> fs = s
+    | Mem f, Res ((Bool,_),r) -> r = List.mem f s
+    | Sort, Res ((Unit,_),r) -> r = ()
+    | To_seq, Res ((Seq Float,_),r) -> Seq.equal (=) r (List.to_seq s)
+    | _, _ -> false
+end
+
+module FloatArraySTM = STM.Make(FAConf)
+
+;;
+Util.set_ci_printing ()
+;;
+QCheck_base_runner.run_tests_main
+  (let count = 1000 in
+   [FloatArraySTM.agree_test         ~count ~name:"STM Float Array test sequential";
+    FloatArraySTM.neg_agree_test_par ~count ~name:"STM Float Array test parallel" 
+])
