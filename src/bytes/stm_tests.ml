bdebda0ed59434e6d148342f7f46a9b22f3bd067
Lucccyo <cha.git@mailo.fr>
2022-09-06 14:38:55 +0200

    clean the code and add copy -- all tests passed

--- src/bytes/stm_tests.ml
+++ src/bytes/stm_tests.ml
@@ -1,6 +1,6 @@
 open QCheck
 open STM
-(* open Util *)
+open Util
 
 (** parallel STM tests of Array *)
 
@@ -10,14 +10,9 @@ struct
     | Length
     | Get of int
     | Set of int * char
-
     | Sub of int * int
-    
-    (* | Copy *)
+    | Copy
     | Fill of int * int * char
-    (* | To_list *)
-    (* | Mem of char *)
-    (* | Sort *)
     | To_seq
   [@@deriving show { with_path = false }]
 
@@ -32,15 +27,9 @@ struct
              [ return Length;
                map (fun i -> Get i) int_gen;
                map2 (fun i c -> Set (i,c)) int_gen char_gen;
-               
                map2 (fun i len -> Sub (i,len)) int_gen int_gen; (* hack: reusing int_gen for length *)
-               
-               (* return Copy; *)
+               return Copy;
                map3 (fun i len c -> Fill (i,len,c)) int_gen int_gen char_gen; (* hack: reusing int_gen for length*)
-               
-               (* return To_list; *)
-               (* map (fun c -> Mem c) char_gen; *)
-               (* return Sort; *)
                return To_seq;
              ])
 
@@ -51,20 +40,13 @@ struct
   let next_state c s = match c with
     | Length -> s
     | Get _  -> s
-    | Set (i,c) ->
-      List.mapi (fun j c' -> if i = j then c else c') s
-    
+    | Set (i,c) -> List.mapi (fun j c' -> if i = j then c else c') s
     | Sub (_,_) -> s
-    
-    (* | Copy -> s *)
+    | Copy -> s
     | Fill (i,l,c) ->
       if i >= 0 && l >= 0 && i+l-1 < (List.length s)
-      then List.mapi (fun j c' -> if i <= j && j <= i+l-1 then c else c') s
-      else s
-      
-    (* | To_list -> s *)
-    (* | Mem _ -> s *)
-    (* | Sort -> List.sort Char.compare s *)
+        then List.mapi (fun j c' -> if i <= j && j <= i+l-1 then c else c') s
+        else s
     | To_seq -> s
 
   let init_sut () = Bytes.make byte_size 'a'
@@ -77,42 +59,30 @@ struct
     | Length       -> Res (int, Bytes.length b)
     | Get i        -> Res (result char exn, protect (Bytes.get b) i)
     | Set (i,c)    -> Res (result unit exn, protect (Bytes.set b i) c)
-    
     | Sub (i,l)    -> Res (result (bytes) exn, protect (Bytes.sub b i) l)
-    
-    (* | Copy         -> Res (bytes, Bytes.copy b) *)
+    | Copy         -> Res (bytes, Bytes.copy b)
     | Fill (i,l,c) -> Res (result unit exn, protect (Bytes.fill b i l) c)
-    (* | To_list      -> Res (list char, Array.to_list a) *)
-    (* | Mem c        -> Res (bool, Array.mem c a) *)
-    (* | Sort         -> Res (unit, Array.sort Char.compare a) *)
     | To_seq       -> Res (seq char, List.to_seq (List.of_seq (Bytes.to_seq b)))
 
   let postcond c (s: char list) res = match c, res with
     | Length, Res ((Int,_),i) -> i = List.length s
     | Get i, Res ((Result (Char,Exn),_), r) ->
       if i < 0 || i >= List.length s
-      then r = Error (Invalid_argument "index out of bounds")
-      else r = Ok (List.nth s i)
+        then r = Error (Invalid_argument "index out of bounds")
+        else r = Ok (List.nth s i)
     | Set (i,_), Res ((Result (Unit,Exn),_), r) -> 
       if i < 0 || i >= List.length s
-      then r = Error (Invalid_argument "index out of bounds")
-      else r = Ok ()
-
+        then r = Error (Invalid_argument "index out of bounds")
+        else r = Ok ()
     | Sub (i,l), Res ((Result (Bytes,Exn),_), r) ->
       if i < 0 || l < 0 || i+l > List.length s
         then r = Error (Invalid_argument "String.sub / Bytes.sub")
         else r = Ok (Bytes.of_seq (List.to_seq (List.filteri (fun j _ -> i <= j && j <= i+l-1) s)))
-      
-    (* | Copy, Res ((Bytes,_),r) ->  = s *)
-
+    | Copy, Res ((Bytes,_),r) -> r = Bytes.of_seq (List.to_seq s)
     | Fill (i,l,_), Res ((Result (Unit,Exn),_), r) ->
       if i < 0 || l < 0 || i+l > List.length s
-      then r = Error (Invalid_argument "String.fill / Bytes.fill" )
-      else r = Ok ()
-
-    (* | To_list, Res ((List Char,_),cs) -> cs = s *)
-    (* | Mem c, Res ((Bool,_),r) -> r = List.mem c s *)
-    (* | Sort, Res ((Unit,_),r) -> r = () *)
+        then r = Error (Invalid_argument "String.fill / Bytes.fill" )
+        else r = Ok ()
     | To_seq, Res ((Seq Char,_),r) -> Seq.equal (=) r (List.to_seq s)
     | _, _ -> false
 end
