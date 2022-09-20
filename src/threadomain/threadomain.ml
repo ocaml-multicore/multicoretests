86e56e8ed4014fdfd743857255035f108b867cc9
Samuel Hym <samuel.hym@rustyne.lautre.net>
2022-09-20 17:19:51 +0200

    Randomize the number of nodes in the domains+threads tests

--- src/threadomain/threadomain.ml
+++ src/threadomain/threadomain.ml
@@ -3,18 +3,15 @@ open QCheck
 (* We mix domains and threads. We use the name _node_ for either a
    domain or a thread *)
 
-(* The global number of nodes that will be spawn *)
-let size = 5 * Domain.recommended_domain_count
-
 let swap arr i j =
   let x = arr.(i) in
   arr.(i) <- arr.(j) ;
   arr.(j) <- x
 
 (** Generate a permutation of [0..size-1] *)
-let permutation s =
-  let arr = Array.init size (fun x -> x) in
-  for i = size - 1 downto 1 do
+let permutation sz s =
+  let arr = Array.init sz (fun x -> x) in
+  for i = sz - 1 downto 1 do
     swap arr i (Gen.int_bound i s)
   done ;
   arr
@@ -23,13 +20,13 @@ let permutation s =
  The tree is represented as an array [a] of integers, [a.(i)] being
  the parent of node [i]. Node [0] is the root of the tree.
  *)
-let tree s =
+let tree sz s =
   let parent i =
     if i == 0
     then -1
     else Gen.int_bound (i-1) s
   in
-  Array.init size parent
+  Array.init sz parent
 
 type worktype = Burn | Tak of int
   [@@deriving show { with_path = false }]
@@ -58,14 +55,14 @@ type spawn_join = {
 (* Ensure that any domain is higher up in the join tree than all its
    threads, so that we cannot have a thread waiting on its domain even
    indirectly *)
-let fix_permutation sj =
+let fix_permutation sz sj =
   let rec dom_of_thd i =
     let candidate = sj.spawn_tree.(i) in
     if candidate = -1 || sj.domain_or.(candidate)
     then candidate
     else dom_of_thd candidate
   in
-  for i = 0 to size-1 do
+  for i = 0 to sz-1 do
     if not sj.domain_or.(i) then
       let i' = sj.join_permutation.(i) in
       let d = dom_of_thd i in
@@ -74,19 +71,19 @@ let fix_permutation sj =
   done ;
   sj
 
-let build_spawn_join spawn_tree join_permutation join_tree domain_or workload =
-  fix_permutation { spawn_tree; join_permutation; join_tree; domain_or; workload }
+let build_spawn_join sz spawn_tree join_permutation join_tree domain_or workload =
+  fix_permutation sz { spawn_tree; join_permutation; join_tree; domain_or; workload }
 
 let worktype =
   let open Gen in
   oneof [pure Burn; map (fun i -> Tak i) (int_bound 200)]
 
-let gen_spawn_join =
+let gen_spawn_join sz =
   let open Gen in
-  build_spawn_join
-    <$> tree <*> permutation <*> tree
-    <*> array_size (pure size) (frequencyl [(1, true); (4, false)])
-    <*> array_size (pure size) worktype
+  build_spawn_join sz
+    <$> tree sz <*> permutation sz <*> tree sz
+    <*> array_size (pure sz) (frequencyl [(1, true); (4, false)])
+    <*> array_size (pure sz) worktype
 
 type handle =
   | NoHdl
@@ -139,8 +136,9 @@ let rec spawn_one sj hdls i =
   Semaphore.Binary.release hdls.available.(sj.join_permutation.(i))
 
 and run_node sj hdls i () =
+  let sz = Array.length sj.spawn_tree in
   (* spawn nodes *)
-  for j = i+1 to size-1 do
+  for j = i+1 to sz-1 do
     if sj.spawn_tree.(j) == i
     then spawn_one sj hdls j
   done ;
@@ -148,23 +146,24 @@ and run_node sj hdls i () =
   work sj.workload.(i) ;
   (* join nodes *)
   let i' = sj.join_permutation.(i) in
-  for j = i'+1 to size-1 do
+  for j = i'+1 to sz-1 do
     if sj.join_tree.(j) == i'
     then join_one hdls j
   done
 
 let run_all_nodes sj =
-  let hdls = { handles = Array.make size NoHdl;
-               available = Array.init size (fun _ -> Semaphore.Binary.make false) } in
+  let sz = Array.length sj.spawn_tree in
+  let hdls = { handles = Array.make sz NoHdl;
+               available = Array.init sz (fun _ -> Semaphore.Binary.make false) } in
   spawn_one sj hdls 0;
   join_one hdls 0;
   (* all the nodes should have been joined now *)
   Array.for_all (fun h -> h = NoHdl) hdls.handles
-   && Atomic.get global = size
+   && Atomic.get global = sz
 
 let main_test = Test.make ~name:"Mash up of threads and domains"
                           ~count:1000
-                          (make ~print:show_spawn_join gen_spawn_join)
+                          (make ~print:show_spawn_join (Gen.sized_size (Gen.int_range 2 100) gen_spawn_join))
                           (Util.fork_prop_with_timeout 1 run_all_nodes)
 
 let _ =
