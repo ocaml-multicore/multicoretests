7b5333e5528227280d250e515beb77b612e87747
Jan Midtgaard <mail@janmidtgaard.dk>
2022-10-27 11:53:05 +0200

    revised stat tests, using Evilstdlib

--- src/neg_tests/stats.ml
+++ src/neg_tests/stats.ml
@@ -1,92 +1,114 @@
-open Lin_api
-
-module Sut_int64 =
-  struct
-    let init () = ref Int64.zero
-    let get r = !r
-    let set r i = r:=i
-    let add r i = let old = !r in r:= Int64.add i old (* buggy: not atomic *)
-    let incr r = add r Int64.one                      (* buggy: not atomic *)
-    let decr r = add r Int64.minus_one                (* buggy: not atomic *)
-end
-
-module Ref_int64_spec : Lin_api.ApiSpec = struct
-  type t = int64 ref
-  let init () = Sut_int64.init ()
-  let cleanup _ = ()
-  let int64 = nat64
-  let api =
-    [ val_ "Sut_int64.get"  Sut_int64.get  (t @-> returning int64);
-      val_ "Sut_int64.set"  Sut_int64.set  (t @-> int64 @-> returning unit);
-      val_ "Sut_int64.add"  Sut_int64.add  (t @-> int64 @-> returning unit);
-      val_ "Sut_int64.incr" Sut_int64.incr (t @-> returning unit);
-      val_ "Sut_int64.decr" Sut_int64.decr (t @-> returning unit);
-    ]
-  end
-
 open QCheck
-module RConf_int64 = struct
-  type t = int64 ref
 
-  type cmd =
-    | Get
-    | Set of int'
-    | Add of int'
-    | Incr
-    | Decr [@@deriving qcheck, show { with_path = false }]
-  and int' = int64 [@gen Gen.(map Int64.of_int nat)]
+let ref_count = (*1000*) 10000
+let clist_count = (*250*) 10000
 
-  let shrink_cmd c = match c with
-    | Get
-    | Incr
-    | Decr -> Iter.empty
-    | Set i -> Iter.map (fun i -> Set i) (Shrink.int64 i)
-    | Add i -> Iter.map (fun i -> Add i) (Shrink.int64 i)
+type stat_test =
+    Stat_test : {
+      name                : string;
+      prop                : 'a -> bool;
+      cmds_gen            : int -> int -> 'a QCheck.arbitrary;
+      count               : int;
+      mutable issue_count : int;
+    } -> stat_test
 
-  type res = RGet of int64 | RSet | RAdd | RIncr | RDecr [@@deriving show { with_path = false }, eq]
+let make_stat_test (Stat_test stat_test) =
+  let adjusted_prop triple =
+    try
+      let res = Util.repeat 100 stat_test.prop triple in
+      if not res then stat_test.issue_count <- 1 + stat_test.issue_count; true
+    with _ -> stat_test.issue_count <- 1 + stat_test.issue_count; true
+  in
+  Test.make ~count:stat_test.count ~name:stat_test.name
+    (stat_test.cmds_gen 20 12) adjusted_prop
 
-  let init () = Sut_int64.init ()
+let test_desc_list = [
+  [ (* ********************** Domain and Lin_api/Lin ********************** *)
+    Stat_test {
+      name        = "Buggy ref int64 programs with Domain + Lin_api";
+      prop        = Lin_tests_common_dsl.RT_int64.lin_prop_domain;
+      cmds_gen    = Lin_tests_common_dsl.RT_int64.arb_cmds_par;
+      count       = ref_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy ref int64 programs with Domain + plain Lin";
+      prop        = Lin_tests_common.RT_int64.lin_prop_domain;
+      cmds_gen    = Lin_tests_common.RT_int64.arb_cmds_par;
+      count       = ref_count;
+      issue_count = 0;
+    };
+  ];
+  [ (* ********************** Thread and Lin_api ********************** *)
+    Stat_test {
+      name        = "Buggy ref int programs with Thread + Lin_api";
+      prop        = Lin_tests_common_dsl.RT_int.lin_prop_thread;
+      cmds_gen    = Lin_tests_common_dsl.RT_int.arb_cmds_par;
+      count       = ref_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy ref int64 programs with Thread + Lin_api";
+      prop        = Lin_tests_common_dsl.RT_int64.lin_prop_thread;
+      cmds_gen    = Lin_tests_common_dsl.RT_int64.arb_cmds_par;
+      count       = ref_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy CList int programs with Thread + Lin_api";
+      prop        = Lin_tests_common_dsl.CLT_int.lin_prop_thread;
+      cmds_gen    = Lin_tests_common_dsl.CLT_int.arb_cmds_par;
+      count       = clist_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy CList int64 programs with Thread + Lin_api";
+      prop        = Lin_tests_common_dsl.CLT_int64.lin_prop_thread;
+      cmds_gen    = Lin_tests_common_dsl.CLT_int64.arb_cmds_par;
+      count       = clist_count;
+      issue_count = 0;
+    };
+  ];
+  [ (* ********************** Thread and plain Lin ********************** *)
+    Stat_test {
+      name        = "Buggy ref int programs with Thread + plain Lin";
+      prop        = Lin_tests_common.RT_int.lin_prop_thread;
+      cmds_gen    = Lin_tests_common.RT_int.arb_cmds_par;
+      count       = ref_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy ref int64 programs with Thread + plain Lin";
+      prop        = Lin_tests_common.RT_int64.lin_prop_thread;
+      cmds_gen    = Lin_tests_common.RT_int64.arb_cmds_par;
+      count       = ref_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy CList int programs with Thread + plain Lin";
+      prop        = Lin_tests_common.CLT_int.lin_prop_thread;
+      cmds_gen    = Lin_tests_common.CLT_int.arb_cmds_par;
+      count       = clist_count;
+      issue_count = 0;
+    };
+    Stat_test {
+      name        = "Buggy CList int64 programs with Thread + plain Lin";
+      prop        = Lin_tests_common.CLT_int64.lin_prop_thread;
+      cmds_gen    = Lin_tests_common.CLT_int64.arb_cmds_par;
+      count       = clist_count;
+      issue_count = 0;
+    };
+  ];
+]
 
-  let run c r = match c with
-    | Get   -> RGet (Sut_int64.get r)
-    | Set i -> (Sut_int64.set r i; RSet)
-    | Add i -> (Sut_int64.add r i; RAdd)
-    | Incr  -> (Sut_int64.incr r; RIncr)
-    | Decr  -> (Sut_int64.decr r; RDecr)
+let print_summary () =
+  Printf.printf "\nStats:\n\n";
+  List.iter (fun sub_list ->
+      List.iter (fun (Stat_test t) ->
+          Printf.printf "%-50s %4i / %4i\n%!" t.name t.issue_count t.count)
+        sub_list;
+      Printf.printf "\n%!")
+    test_desc_list
 
-  let cleanup _ = ()
-end
-
-module RT_int64_DSL = Lin_api.Make(Ref_int64_spec)
-module RT_int64_LIN = Lin.Make(RConf_int64)
-
-
-let buggy cpt prop pg =
-  try
-    let res = Util.repeat 100 prop pg in
-    if not res then incr cpt; true
-  with _ -> incr cpt; true
-
-let dsl = ref 0
-let lin = ref 0
-
-let buggy_dsl = buggy dsl RT_int64_DSL.lin_prop_thread
-let buggy_lin = buggy lin RT_int64_LIN.lin_prop_thread
-
-let count = 10000
-
-let test_dsl = Test.make
-                 ~name:"Number of buggy programs with original dsl"
-                 ~count
-                 (RT_int64_DSL.arb_cmds_par 20 10)
-                 buggy_dsl
-
-let test_lin = Test.make
-                 ~name:"Number of buggy programs with original lin"
-                 ~count
-                 (RT_int64_LIN.arb_cmds_par 20 10)
-                 buggy_lin
-
-let _ = QCheck_runner.run_tests ~verbose:false [ test_dsl; test_lin ]
-let _ = Printf.printf "Buggy programs with original lin: %i / %i\n%!" (!lin) count
-let _ = Printf.printf "Buggy programs with dsl:          %i / %i\n%!" (!dsl) count
+let _ = QCheck_runner.run_tests ~verbose:true (List.map make_stat_test (List.concat test_desc_list))
+let () = print_summary ()
