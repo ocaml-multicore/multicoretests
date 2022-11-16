5c93e00deb08f21f0f1860043f55e6d168c302f4
Jan Midtgaard <mail@janmidtgaard.dk>
2022-11-16 11:06:51 +0100

    reduce test count for STM int ref Thread test

--- src/neg_tests/stm_tests_thread_ref.ml
+++ src/neg_tests/stm_tests_thread_ref.ml
@@ -8,7 +8,6 @@ then
   Printf.printf "STM ref tests with Thread disabled under bytecode\n\n%!"
 else
 QCheck_runner.run_tests_main
-  (let count = 1000 in
-   [RT_int.agree_test_conc       ~count ~name:"STM int ref test with Thread";
-    RT_int64.neg_agree_test_conc ~count ~name:"STM int64 ref test with Thread";
-   ])
+  [RT_int.agree_test_conc       ~count:250  ~name:"STM int ref test with Thread";
+   RT_int64.neg_agree_test_conc ~count:1000 ~name:"STM int64 ref test with Thread";
+  ]
