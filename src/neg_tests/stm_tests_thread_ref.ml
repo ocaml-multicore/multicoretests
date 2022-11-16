cefa9cbe73adb6f1a9757a44e4a093558ac1d4f2
Jan Midtgaard <mail@janmidtgaard.dk>
2022-11-16 10:36:17 +0100

    disable STM Thread test under bytecode

--- src/neg_tests/stm_tests_thread_ref.ml
+++ src/neg_tests/stm_tests_thread_ref.ml
@@ -3,6 +3,10 @@ open Stm_tests_spec_ref
 module RT_int   = STM_thread.Make(RConf_int)
 module RT_int64 = STM_thread.Make(RConf_int64)
 ;;
+if Sys.backend_type = Sys.Bytecode
+then
+  Printf.printf "STM ref tests with Thread disabled under bytecode\n\n%!"
+else
 QCheck_runner.run_tests_main
   (let count = 1000 in
    [RT_int.agree_test_conc       ~count ~name:"STM int ref test with Thread";
