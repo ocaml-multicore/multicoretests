48fd18f00d313c750e5f53c8d3fcc496cb498b09
n-osborne <nicolas.osborne@tarides.com>
2022-11-22 14:18:00 +0100

    update section on parallel testing
    - less mention of `Thread` as it is fairly experimental atm
    - remove the mention and code snippet for dune file (not the point here)
    - one section for all parallel testing (buggy and fixed) so that the
      section demonstrate how it is easy to do and how it nicely catch
      parallel bugs (as data races)

--- stm-doc/mutable_set_v3.ml
+++ stm-doc/mutable_set_v3.ml
@@ -1,92 +1,104 @@
+module type S = sig
+  type elt
+  type t
+  val empty    : unit -> t
+  val mem      : elt -> t -> bool
+  val add      : elt -> t -> unit
+  val cardinal : t -> int
+end
+
+module Lib : sig
+  module Make : functor (Ord : Set.OrderedType) -> S with type elt = Ord.t
+end
+= struct
+  module Make (Ord : Set.OrderedType) =
+    struct
+    module S = Set.Make (Ord)
+
+    type elt = Ord.t
+
+    type t = {
+      mutable content  : S.t;
+      mutable cardinal : int;
+      mutex            : Mutex.t}
+
+    let empty () = { content = S.empty; cardinal = 0; mutex = Mutex.create () }
+
+    let mem_non_lock a t = S.mem a t.content
+
+    let mem a t =
+      Mutex.lock t.mutex;
+      let b = mem_non_lock a t in
+      Mutex.unlock t.mutex;
+      b
+
+    let add a t =
+      Mutex.lock t.mutex;
+      if not (mem_non_lock a t)
+      then begin
+        t.content <- S.add a t.content;
+        t.cardinal <- t.cardinal + 1;
+      end;
+      Mutex.unlock t.mutex
+
+    let cardinal t =
+      Mutex.lock t.mutex;
+      let c = t.cardinal in
+      Mutex.unlock t.mutex;
+      c
+  end
+end
+
 open QCheck
 open STM_base
 
-module Lib : sig
-  type 'a t
-  val empty  : unit -> 'a t
-  val mem    : 'a -> 'a t -> bool
-  val add    : 'a -> 'a t -> unit
-  val length : 'a t -> int
-end
-= struct
-  type 'a t = {
-    mutable content : 'a list;
-    mutable length  : int;
-    mutex           : Mutex.t}
-
-  let empty () = { content = []; length = 0; mutex = Mutex.create () }
-
-  let mem_non_lock a t = List.mem a t.content
-
-  let mem a t =
-    Mutex.lock t.mutex;
-    let b = mem_non_lock a t in
-    Mutex.unlock t.mutex;
-    b
-
-  let add a t =
-    Mutex.lock t.mutex;
-    if not (mem_non_lock a t)
-    then begin
-      t.content <- a :: t.content;
-      t.length  <- t.length + 1;
-    end;
-    Mutex.unlock t.mutex
-
-  let length t =
-    Mutex.lock t.mutex;
-    let l = t.length in
-    Mutex.unlock t.mutex;
-    l
-end
-
-
 module Lib_spec : Spec = struct
 
-  module State = Set.Make (struct type t = int let compare = Int.compare end)
+  module S = Lib.Make (Int)
 
-  type sut        = int Lib.t
-  let init_sut () = Lib.empty ()
-  let cleanup _   = ()
+  type sut = S.t
+  let init_sut () = S.empty ()
+  let cleanup _ = ()
 
   type cmd =
     | Mem of int
     | Add of int
-    | Length [@@deriving show { with_path = false }]
+    | Cardinal [@@deriving show { with_path = false }]
 
-  type state     = State.t
-  let init_state = State.empty
+  let run cmd sut =
+    match cmd with
+    | Mem i    -> Res (bool, S.mem i sut)
+    | Add i    -> Res (unit, S.add i sut)
+    | Cardinal -> Res (int, S.cardinal sut)
+
+  type state = int list
+  let init_state = []
+
+  let next_state cmd state =
+    match cmd with
+    | Mem _    -> state
+    | Add i    -> if List.mem i state then state else i :: state
+    | Cardinal -> state
+
+  let precond _cmd _state = true
+
+  let postcond cmd state res =
+    match cmd, res with
+    | Mem i,  Res ((Bool,_), b)  -> b = List.mem i state
+    | Cardinal, Res ((Int,_), l) -> l = List.length state
+    | Add _,  Res ((Unit,_),_)   -> true
+    | _                          -> false
 
   let arb_cmd _state =
     QCheck.make ~print:show_cmd
       (QCheck.Gen.oneof
-        [Gen.return Length;
+        [Gen.return Cardinal;
          Gen.map (fun i -> Mem i) Gen.int;
          Gen.map (fun i -> Add i) Gen.int;
         ])
-
-  let next_state cmd state =
-    match cmd with
-    | Mem _  -> state
-    | Add i  -> State.add i state
-    | Length -> state
-
-  let run cmd sut =
-    match cmd with
-    | Mem i  -> Res (bool, Lib.mem i sut)
-    | Add i  -> Res (unit, Lib.add i sut)
-    | Length -> Res (int, Lib.length sut)
-
-  let precond _ _ = true
-
-  let postcond cmd state res =
-    match cmd, res with
-    | Mem i,  Res ((Bool,_), b) -> b = State.mem i state
-    | Length, Res ((Int,_), l)  -> l = State.cardinal state
-    | Add _,  Res ((Unit,_),_)  -> true
-    | _                         -> false
 end
 
 module Lib_domain = STM_domain.Make(Lib_spec)
 
-let _ = QCheck_runner.run_tests ~verbose:true [Lib_domain.agree_test_par ~count:100 ~name:"STM parallel tests"]
+let _ = QCheck_runner.run_tests ~verbose:true [Lib_domain.agree_test_par ~count:100 ~name:"STM sequential tests"]
+
