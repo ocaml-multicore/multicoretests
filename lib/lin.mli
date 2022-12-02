842cf0833a097967089c50b7994a2283e6f72d91
Samuel Hym <samuel.hym@rustyne.lautre.net>
2022-12-02 18:49:28 +0100

    Add some links in Lin documentation

--- lib/lin.mli
+++ lib/lin.mli
@@ -22,7 +22,7 @@ module Internal : sig
 
     val shrink_cmd : cmd QCheck.Shrink.t
     (** A command shrinker.
-        To a first approximation you can use [Shrink.nil]. *)
+        To a first approximation you can use {!QCheck.Shrink.nil}. *)
 
     type res
     (** The command result type *)
@@ -69,29 +69,29 @@ type deconstructible = |
 
 type combinable
 (** Type definition to denote that a described type can be composed with
-    other combinators such as [list]. *)
+    other combinators such as {!list}. *)
 
 type noncombinable
 (** Type definition to denote that a described type cannot be composed with
-    other combinators such as [list]. *)
+    other combinators such as {!list}. *)
 
 type (_, _, _, _) ty
 (** Type definition for type-describing combinators.
     [(typ,con,styp,comb) ty] represents a type [typ] and with an underlying state of type [styp].
-    The [con] type parameter indicates whether the combinator type is [constructible] or [deconstructible].
-    The [comb] type parameter indicates whether the combinator type is [combinable] or [noncombinable].
+    The [con] type parameter indicates whether the combinator type is {!constructible} or {!deconstructible}.
+    The [comb] type parameter indicates whether the combinator type is {!combinable} or {!noncombinable}.
 *)
 
 val gen : 'a QCheck.arbitrary -> ('a -> string) -> ('a, constructible, 's, combinable) ty
-(** [gen arb to_str] builds a [constructible] and [combinable] type combinator
+(** [gen arb to_str] builds a {!constructible} and {!combinable} type combinator
     from a QCheck generator [arb] and a to-string function [to_str]. *)
 
 val deconstructible : ('a -> string) -> ('a -> 'a -> bool) -> ('a, deconstructible, 's, combinable) ty
-(** [deconstructible to_str eq] builds a [deconstructible] and [combinable] type combinator
+(** [deconstructible to_str eq] builds a {!deconstructible} and {!combinable} type combinator
     from a to-string function [to_str] and an equality predicate [eq]. *)
 
 val gen_deconstructible : 'a QCheck.arbitrary -> ('a -> string) -> ('a -> 'a -> bool) -> ('a, 'c, 's, combinable) ty
-(** [gen_deconstructible arb to_str eq] builds a [combinable] type combinator
+(** [gen_deconstructible arb to_str eq] builds a {!combinable} type combinator
     from a QCheck generator [arb], a to-string function [to_str] and an
     equality predicate [eq]. *)
 
@@ -99,86 +99,86 @@ val gen_deconstructible : 'a QCheck.arbitrary -> ('a -> string) -> ('a -> 'a -> 
 (** {2 Type combinators} *)
 
 val unit : (unit, 'a, 'b, combinable) ty
-(** The [unit] combinator represents the [unit] type *)
+(** The [unit] combinator represents the {{!Stdlib.Unit.t}[unit]} type *)
 
 val bool : (bool, 'a, 'b, combinable) ty
-(** The [bool] combinator represents the [bool] type *)
+(** The [bool] combinator represents the {{!Stdlib.Bool.t}[bool]} type *)
 
 val char : (char, 'a, 'b, combinable) ty
-(** The [char] combinator represents the [char] type.
-    It uses a uniform generator based on [QCheck.char]. *)
+(** The [char] combinator represents the {{!Stdlib.Char.t}[char]} type.
+    It uses a uniform generator based on {!QCheck.char}. *)
 
 val char_printable : (char, 'a, 'b, combinable) ty
-(** The [char_printable] combinator represents the [char] type.
+(** The [char_printable] combinator represents the {{!Stdlib.Char.t}[char]} type.
     The generated characters have character codes 32-126 or 10 (newline)
-    and are based on [QCheck.printable_char]. *)
+    and are based on {!QCheck.printable_char}. *)
 
 val nat_small : (int, 'a, 'b, combinable) ty
-(** The [nat_small] combinator represents the [int] type.
+(** The [nat_small] combinator represents the {{!Stdlib.Int.t}[int]} type.
     The generated integers are non-negative, less than 100,
-    and are based on [QCheck.small_nat]. *)
+    and are based on {!QCheck.small_nat}. *)
 
 val int : (int, 'a, 'b, combinable) ty
-(** The [int] combinator represents the [int] type.
-    It uses a uniform generator based on [QCheck.int]. *)
+(** The [int] combinator represents the {{!Stdlib.Int.t}[int]} type.
+    It uses a uniform generator based on {!QCheck.int}. *)
 
 val int_small : (int, 'a, 'b, combinable) ty
-(** The [int_small] combinator represents the [int] type.
+(** The [int_small] combinator represents the {{!Stdlib.Int.t}[int]} type.
     The generated integers are non-negative
-    and are based on [QCheck.small_int]. *)
+    and are based on {!QCheck.small_int}. *)
 
 val int_pos : (int, 'a, 'b, combinable) ty
-(** The [int_pos] combinator represents the [int] type.
+(** The [int_pos] combinator represents the {{!Stdlib.Int.t}[int]} type.
     The generated integers are non-negative
-    and uniformly distributed. It is based on [QCheck.pos_int]. *)
+    and uniformly distributed. It is based on {!QCheck.pos_int}. *)
 
 val int_bound : int -> (int, 'a, 'b, combinable) ty
-(** The [int_bound b] combinator represents the [int] type.
+(** The [int_bound b] combinator represents the {{!Stdlib.Int.t}[int]} type.
     The generated integers range from [0] to [b], inclusive.
-    It uses a uniform generator based on [QCheck.int_bound]. *)
+    It uses a uniform generator based on {!QCheck.int_bound}. *)
 
 val int32 : (Int32.t, 'a, 'b, combinable) ty
-(** The [int32] combinator represents the [int32] type.
-    It uses a uniform generator based on [QCheck.int32]. *)
+(** The [int32] combinator represents the {{!Stdlib.Int32.t}[int32]} type.
+    It uses a uniform generator based on {!QCheck.int32}. *)
 
 val int64 : (Int64.t, 'a, 'b, combinable) ty
-(** The [int64] combinator represents the [int64] type.
-    It uses a uniform generator based on [QCheck.int64]. *)
+(** The [int64] combinator represents the {{!Stdlib.Int64.t}[int64]} type.
+    It uses a uniform generator based on {!QCheck.int64}. *)
 
 val nat64_small : (Int64.t, 'a, 'b, combinable) ty
-(** The [nat64_small] combinator represents the [int64] type.
+(** The [nat64_small] combinator represents the {{!Stdlib.Int64.t}[int64]} type.
     The generated integers are non-negative
-    and are based on [QCheck.small_nat]. *)
+    and are based on {!QCheck.small_nat}. *)
 
 val float : (float, 'a, 'b, combinable) ty
-(** The [float] combinator represents the [float] type.
+(** The [float] combinator represents the {{!Stdlib.Float.t}[float]} type.
     The generated floating point numbers do not include nan and infinities.
-    It is based on [QCheck.float]. *)
+    It is based on {!QCheck.float}. *)
 
 val string : (String.t, 'a, 'b, combinable) ty
-(** The [string] combinator represents the [string] type.
-    The generated strings have a size generated from [QCheck.Gen.nat] and
-    characters resulting from [QCheck.char]. It is based on [QCheck.string]. *)
+(** The [string] combinator represents the {{!Stdlib.String.t}[string]} type.
+    The generated strings have a size generated from {!QCheck.Gen.nat} and
+    characters resulting from {!QCheck.char}. It is based on {!QCheck.string}. *)
 
 val string_small : (String.t, 'a, 'b, combinable) ty
-(** The [small_string] combinator represents the [string] type.
-    The generated strings have a size generated from [QCheck.Gen.small_nat] and
-    characters resulting from [QCheck.char]. It is based on [QCheck.small_string]. *)
+(** The [small_string] combinator represents the {{!Stdlib.String.t}[string]} type.
+    The generated strings have a size generated from {!QCheck.Gen.small_nat} and
+    characters resulting from {!QCheck.char}. It is based on {!QCheck.small_string}. *)
 
 val string_small_printable : (String.t, 'a, 'b, combinable) ty
-(** The [small_string] combinator represents the [string] type.
-    The generated strings have a size generated from [QCheck.Gen.small_nat] and
-    characters resulting from [QCheck.printable_char].
-    It is based on [QCheck.small_printable_string]. *)
+(** The [small_string] combinator represents the {{!Stdlib.String.t}[string]} type.
+    The generated strings have a size generated from {!QCheck.Gen.small_nat} and
+    characters resulting from {!QCheck.printable_char}.
+    It is based on {!QCheck.small_printable_string}. *)
 
 val option :
   ?ratio:float ->
   ('a, 'c, 's, combinable) ty -> ('a option, 'c, 's, combinable) ty
-(** The [option] combinator represents the [option] type.
+(** The [option] combinator represents the {{!Stdlib.Option.t}[option]} type.
     The generated values from [option t] are either [Some v] or [None]
     with [v] being generated by the [t] combinator.
     An optional [ratio] allows to change the default [0.85] [Some]s.
-    It is based on [QCheck.option]. *)
+    It is based on {!QCheck.option}. *)
 
 val opt :
   ?ratio:float ->
@@ -186,38 +186,38 @@ val opt :
 (** The [opt] combinator is an alias for {!option}. *)
 
 val list : ('a, 'c, 's, combinable) ty -> ('a list, 'c, 's, combinable) ty
-(** The [list] combinator represents the [list] type.
-    The generated lists from [list t] have a length resulting from [QCheck.Gen.nat]
+(** The [list] combinator represents the {{!Stdlib.List.t}[list]} type.
+    The generated lists from [list t] have a length resulting from {!QCheck.Gen.nat}
     and have their elements generated by the [t] combinator.
-    It is based on [QCheck.list]. *)
+    It is based on {!QCheck.list}. *)
 
 val array : ('a, 'c, 's, combinable) ty -> ('a array, 'c, 's, combinable) ty
-(** The [array] combinator represents the [array] type.
-    The generated arrays from [array t] have a length resulting from [QCheck.Gen.nat]
+(** The [array] combinator represents the {{!Stdlib.Array.t}[array]} type.
+    The generated arrays from [array t] have a length resulting from {!QCheck.Gen.nat}
     and have their elements generated by the [t] combinator.
-    It is based on [QCheck.array]. *)
+    It is based on {!QCheck.array}. *)
 
 val seq : ('a, 'c, 's, combinable) ty -> ('a Seq.t, 'c, 's, combinable) ty
-(** The [seq] combinator represents the [Seq.t] type.
-    The generated sequences from [seq t] have a length resulting from [QCheck.Gen.nat]
+(** The [seq] combinator represents the {!Stdlib.Seq.t} type.
+    The generated sequences from [seq t] have a length resulting from {!QCheck.Gen.nat}
     and have their elements generated by the [t] combinator. *)
 
 val t : ('a, constructible, 'a, noncombinable) ty
-(** The [t] combinator represents the type [Spec.t] of the system under test. *)
+(** The [t] combinator represents the type {!Spec.t} of the system under test. *)
 
 val state : ('a, constructible, 'a, noncombinable) ty
-(** The [state] combinator represents the type [ApiSpec.t] of the system under test.
+(** The [state] combinator represents the type {!Spec.t} of the system under test.
     It is an alias for the [t] combinator. *)
 
 val or_exn :
   ('a, deconstructible, 'b, combinable) ty ->
   (('a, exn) result, deconstructible, 'c, combinable) ty
 (** The [or_exn] combinator transforms a result type representing [t]
-    into a [(t, exn) result] type. *)
+  into a [(t, exn)] {{!Stdlib.Result.t}[result]} type. *)
 
 val print_result :
   ('a -> string) -> ('b -> string) -> ('a, 'b) result -> string
-(** [print_result pa pb] creates a to-string function for a [(a,b) result] type
+(** [print_result pa pb] creates a to-string function for a [(a,b)] {{!Stdlib.Result.t}[result]} type
     given two to-string functions for [a]s and [b]s, respectively. *)
 
 val print : ('a, 'c, 's, 'comb) ty -> 'a -> string
@@ -274,7 +274,7 @@ val val_ : string -> 'f -> ('f, 'r, 's) Fun.fn -> int * 's elem
 
 val val_freq : int -> string -> 'f -> ('f, 'r, 's) Fun.fn -> int * 's elem
 (** [val_freq w str f sig] describes a function signature like
-    [val_ str f sig] but with relative weight [w] rather than 1.
+    {!val_} [str f sig] but with relative weight [w] rather than 1.
     A function of weight 2 will have twice the probability of being
     invoked compared to a function of weight 1. *)
 
@@ -300,7 +300,7 @@ end
 
 module MakeCmd (Spec : Spec) : Internal.CmdSpec [@alert "-internal"]
 (** Functor to map a combinator-based module signature description
-    into a raw [Lin] description.
+    into a raw {!Lin} description.
     This functor is exposed for internal uses only, its API may change
     at any time.
     *)
--- lib/lin_effect.mli
+++ lib/lin_effect.mli
@@ -1,6 +1,6 @@
 open Lin
 
-(** functor to build an internal module representing effect-based tests *)
+(** functor to build an internal module representing {!Stdlib.Effect}-based tests *)
 module Make_internal (Spec : Internal.CmdSpec [@alert "-internal"]) : sig
   module EffSpec : sig
     type cmd
@@ -12,24 +12,24 @@ end
   [@@alert internal "This module is exposed for internal uses only, its API may change at any time"]
 
 val fork : (unit -> unit) -> unit
-(** Helper function to fork a process in the underlying [Effect-based] scheduler
+(** Helper function to fork a process in the underlying {!Stdlib.Effect}-based scheduler
 *)
 
 val yield : unit -> unit
-(** Helper function to yield control in the underlying [Effect-based] scheduler
+(** Helper function to yield control in the underlying {!Stdlib.Effect}-based scheduler
 *)
 
 (** functor to build a module for [Effect]-based testing *)
 module Make (Spec : Spec) : sig
   val lin_test : count:int -> name:string -> QCheck.Test.t
-  (** [lin_test ~count:c ~name:n] builds an [Effect]-based test with the name
+  (** [lin_test ~count:c ~name:n] builds an {!Stdlib.Effect}-based test with the name
       [n] that iterates [c] times. The test fails if one of the generated
       programs is not sequentially consistent. In that case it fails, and prints
       a reduced counter example.
   *)
 
   val neg_lin_test : count:int -> name:string -> QCheck.Test.t
-  (** [neg_lin_test ~count:c ~name:n] builds a negative [Effect]-based test with
+  (** [neg_lin_test ~count:c ~name:n] builds a negative {!Stdlib.Effect}-based test with
       the name [n] that iterates [c] times. The test fails if no counter example
       is found, and succeeds if a counter example is indeed found, and prints it
       afterwards.
