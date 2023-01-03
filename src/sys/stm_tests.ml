e1914c53a965a3556609c19a67081479f815df9f
Lucccyo <cha.git@mailo.fr>
2023-01-03 13:57:42 +0100

    separate para tests for unix based and negative para tests for windows

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -240,6 +240,8 @@ module Sys_dom = STM_domain.Make(SConf)
 
 ;;
 QCheck_base_runner.run_tests_main [
-    Sys_seq.agree_test     ~count:1000  ~name:"STM Sys test sequential";
-    Sys_dom.agree_test_par ~count:200   ~name:"STM Sys test parallel"
+    Sys_seq.agree_test   ~count:1000   ~name:"STM Sys test sequential";
+    if Sys.win32
+    then Sys_dom.neg_agree_test_par   ~count:1000   ~name:"STM Sys test parallel"
+    else Sys_dom.agree_test_par       ~count:1000   ~name:"STM Sys test parallel"
   ]
