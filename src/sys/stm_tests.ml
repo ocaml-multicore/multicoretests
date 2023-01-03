82448aeab399998cf0af9e87c663939b7b235668
Lucccyo <cha.git@mailo.fr>
2023-01-03 10:34:13 +0100

    make touch's postcond 'true' bc iit is not a feature tested

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -231,8 +231,8 @@ struct
           | Some l ->
             List.sort String.compare l
             = List.sort String.compare (Array.to_list array_of_subdir))))
-    | Touch (path, new_file_name, _perm), Res ((Int,_),n) ->
-      let complete_path = (path @ [new_file_name]) in
+    | Touch (_path, _new_file_name, _perm), Res ((Int,_),_) -> true
+      (* let complete_path = (path @ [new_file_name]) in
       if Sys.win32
       then (
         if n = 0
@@ -248,7 +248,7 @@ struct
           (mem_model fs path && path_is_a_dir fs path && not (mem_model fs complete_path)) ||
           mem_model fs complete_path
         )
-        else (not (mem_model fs path) || not (path_is_a_dir fs path)) (* the path is incorrect*)
+        else (not (mem_model fs path) || not (path_is_a_dir fs path)) the path is incorrect *)
     | _,_ -> false
 end
 
