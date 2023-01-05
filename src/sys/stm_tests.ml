f85c1c117b6e90a8700698f5975ce2b0c20d4684
Lucccyo <cha.git@mailo.fr>
2023-01-05 15:47:40 +0100

    rerun CI tests

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -243,5 +243,5 @@ QCheck_base_runner.run_tests_main [
     Sys_seq.agree_test   ~count:1000   ~name:"STM Sys test sequential";
     if Sys.win32
     then Sys_dom.neg_agree_test_par   ~count:1000   ~name:"STM Sys test parallel"
-    else Sys_dom.agree_test_par       ~count:1000   ~name:"STM Sys test parallel"
+    else Sys_dom.agree_test_par       ~count:1001   ~name:"STM Sys test parallel"
   ]
