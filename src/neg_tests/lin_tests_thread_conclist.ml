a9b2cc1ff9dd28e388b9e9a212dc5c895ad4e476
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-01-23 14:42:57 +0100

    Disable a few other Thread tests under bytecode

--- src/neg_tests/lin_tests_thread_conclist.ml
+++ src/neg_tests/lin_tests_thread_conclist.ml
@@ -5,8 +5,16 @@ open Lin_tests_common
 module CLT_int_thread = Lin_thread.Make_internal(CLConf (Int)) [@alert "-internal"]
 module CLT_int64_thread = Lin_thread.Make_internal(CLConf (Int64)) [@alert "-internal"]
 
-;;
-QCheck_base_runner.run_tests_main
-  (let count = 1000 in
-   [CLT_int_thread.lin_test   ~count ~name:"Lin CList int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
-    CLT_int64_thread.lin_test ~count ~name:"Lin CList int64 test with Thread"]) (* not triggering context switch, unfortunately *)
+let _ =
+  let count = 1000 in
+  let tests = [
+    CLT_int_thread.lin_test   ~count ~name:"Lin CList int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
+    CLT_int64_thread.lin_test ~count ~name:"Lin CList int64 test with Thread" (* not triggering context switch, unfortunately *)
+  ] in
+  let tests =
+    if Sys.backend_type = Sys.Bytecode then (
+      Printf.printf "Lin CList int64 test with Thread disabled under bytecode\n\n%!";
+      [ List.hd tests ])
+    else tests
+  in
+  QCheck_base_runner.run_tests_main tests
