c12c1d01303a786aa8898297cb8e6e122a2032f0
Jan Midtgaard <mail@janmidtgaard.dk>
2023-02-02 15:47:22 +0100

    wrap child threads in a result type to ensure clean-up on exceptions

--- lib/lin_thread.ml
+++ lib/lin_thread.ml
@@ -20,14 +20,16 @@ module Make_internal (Spec : Internal.CmdSpec [@alert "-internal"]) = struct
   (* Linearization property based on [Thread] *)
   let lin_prop (seq_pref, cmds1, cmds2) =
     let sut = Spec.init () in
-    let obs1, obs2 = ref [], ref [] in
+    let obs1, obs2 = ref (Ok []), ref (Ok []) in
     let pref_obs = interp_plain sut seq_pref in
     let wait = ref true in
-    let th1 = Thread.create (fun () -> while !wait do Thread.yield () done; obs1 := interp_thread sut cmds1) () in
-    let th2 = Thread.create (fun () -> wait := false; obs2 := interp_thread sut cmds2) () in
+    let th1 = Thread.create (fun () -> while !wait do Thread.yield () done; obs1 := try Ok (interp_thread sut cmds1) with exn -> Error exn) () in
+    let th2 = Thread.create (fun () -> wait := false; obs2 := try Ok (interp_thread sut cmds2) with exn -> Error exn) () in
     Thread.join th1;
     Thread.join th2;
     Spec.cleanup sut;
+    let obs1 = match !obs1 with Ok v -> ref v | Error exn -> raise exn in
+    let obs2 = match !obs2 with Ok v -> ref v | Error exn -> raise exn in
     let seq_sut = Spec.init () in
     (* we reuse [check_seq_cons] to linearize and interpret sequentially *)
     check_seq_cons pref_obs !obs1 !obs2 seq_sut []
