1e39e8180d5e22273cdde2f02c148b4f198b7b64
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-03-22 18:50:38 +0100

    Disable the negative Out_channel tests on Windows and macOS
    These tests are not triggering consistently on those platforms so we
    disable them for now

--- src/io/lin_tests_dsl_domain.ml
+++ src/io/lin_tests_dsl_domain.ml
@@ -7,8 +7,14 @@ open Lin_tests_dsl_common_io.Lin_tests_dsl_common
 module IC_domain = Lin_domain.Make(ICConf)
 module OC_domain = Lin_domain.Make(OCConf)
 
-let _ =
-  QCheck_base_runner.run_tests_main [
-    IC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL In_channel test with Domain";
+let tests =
+  IC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL In_channel test with Domain" ::
+  if Sys.os_type = "Win32" || Sys.getenv_opt "OCAML_SYSTEM" = Some "macosx"
+  then (
+    Printf.printf "Lin DSL Out_channel test with Domain disabled under Windows and macOS\n\n%!";
+    []
+  ) else [
     OC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL Out_channel test with Domain";
   ]
+
+let _ = QCheck_base_runner.run_tests_main tests
