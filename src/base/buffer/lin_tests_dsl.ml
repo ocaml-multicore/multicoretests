23409f072995b277bbe92205b138bfc243a77e51
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-04-05 14:59:58 +0200

    Add Lin tests for Base.Buffer
    These tests are enabled only when base is installed to avoid adding
    another global dependency
    This still requires ensuring that the test can compile without Base
    because dune does not support `enabled_if %{lib-available:base}` on
    executables (and it doesn't disable compilation in `test` stanza)

---
+++ src/base/buffer/lin_tests_dsl.ml
@@ -0,0 +1,48 @@
+module Buffer = BaseOrNoBase.Buffer
+
+module BBConf = struct
+  type t = Buffer.t
+
+  let init () = Buffer.create 8
+  let cleanup _ = ()
+
+  open Lin
+  let int = nat_small
+  let char = char_printable
+  let string = string_small_printable
+
+  let api =
+    [
+      (* val_ "Buffer.sexp_of_t" Buffer.sexp_of_t (t @-> returning Sexplib0.Sexp.t); *)
+          (* Disabled due to missing Sexplib0.Sexp.t *)
+      (* val_ "Buffer.create" Buffer.create (int @-> returning t); *)
+          (* Disabled as returning t *)
+
+      val_ "Buffer.contents" Buffer.contents (t @-> returning string);
+
+      (* val_ "Buffer.contents_bytes" Buffer.contents_bytes (t @-> returning bytes); *)
+          (* Disabled due to missing bytes *)
+
+      val_ "Buffer.nth" Buffer.nth (t @-> int @-> returning_or_exc char);
+      val_ "Buffer.length" Buffer.length (t @-> returning int);
+      val_ "Buffer.clear" Buffer.clear (t @-> returning unit);
+      val_ "Buffer.reset" Buffer.reset (t @-> returning unit);
+      val_ "Buffer.add_char" Buffer.add_char (t @-> char @-> returning unit);
+      val_ "Buffer.add_string" Buffer.add_string (t @-> string @-> returning unit);
+
+      (* val_ "Buffer.add_substring" Buffer.add_substring (t @-> string @-> int @-> int @-> returning_or_exc unit); *)
+          (* Disabled due to missing labels *)
+
+      (* val_ "Buffer.add_bytes" Buffer.add_bytes (t @-> bytes @-> returning unit); *)
+      (* val_ "Buffer.add_subbytes" Buffer.add_subbytes (t @-> bytes @-> int @-> int @-> returning unit); *)
+          (* Disabled due to missing bytes *)
+
+      (* val_ "Buffer.add_buffer" Buffer.add_buffer (t @-> t @-> unit); *)
+    ]
+end
+
+module BBT_domain = Lin_domain.Make(BBConf)
+
+let _ =
+  QCheck_base_runner.run_tests_main
+    [ BBT_domain.neg_lin_test ~count:1000 ~name:"Lin DSL Base Buffer test with Domain" ]
