524503c43a100b0b138671f49d138ae8f09b5f41
Jan Midtgaard <mail@janmidtgaard.dk>
2023-04-18 16:21:08 +0200

    Use package qcheck-core and its QCheck_base_runner

--- src/neg_tests/stm_tests_domain_ref.ml
+++ src/neg_tests/stm_tests_domain_ref.ml
@@ -3,7 +3,7 @@ open Stm_tests_spec_ref
 module RT_int   = STM_domain.Make(RConf_int)
 module RT_int64 = STM_domain.Make(RConf_int64)
 ;;
-QCheck_runner.run_tests_main
+QCheck_base_runner.run_tests_main
   (let count = 1000 in
    [RT_int.neg_agree_test_par        ~count ~name:"STM int ref test parallel";
     RT_int64.neg_agree_test_par      ~count ~name:"STM int64 ref test parallel";
