a3a67a79191ff87d075e7563ccf14e2dbe35caad
Jan Midtgaard <mail@janmidtgaard.dk>
2023-07-14 17:04:47 +0200

    Bump asym count to account for sensitive GA runners

--- src/neg_tests/stm_tests_domain_ref_asym.ml
+++ src/neg_tests/stm_tests_domain_ref_asym.ml
@@ -4,6 +4,6 @@ module RT_int   = STM_domain.Make(RConf_int)
 module RT_int64 = STM_domain.Make(RConf_int64)
 ;;
 QCheck_base_runner.run_tests_main
-  [RT_int.neg_agree_test_par_asym   ~count:2000 ~name:"STM int ref test parallel asymmetric";
-   RT_int64.neg_agree_test_par_asym ~count:2000 ~name:"STM int64 ref test parallel asymmetric";
+  [RT_int.neg_agree_test_par_asym   ~count:5000 ~name:"STM int ref test parallel asymmetric";
+   RT_int64.neg_agree_test_par_asym ~count:5000 ~name:"STM int64 ref test parallel asymmetric";
   ]
