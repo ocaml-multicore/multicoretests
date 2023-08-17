2433827343b1a80030dae97943fe969403cd70fe
Jan Midtgaard <mail@janmidtgaard.dk>
2023-08-17 11:23:46 +0200

    Remove lift to reduce allocations

--- src/io/lin_tests_spec_io.ml
+++ src/io/lin_tests_spec_io.ml
@@ -65,15 +65,17 @@ end
 module OCConf : Lin.Spec = struct
   (* a path and an open channel to that file; we need to keep the path
      to cleanup after the test run *)
-  type t = string * Out_channel.t
+  type t = Out_channel.t
+  let path = ref ""
 
-  let init () = Filename.open_temp_file "lin-dsl-" ""
-  let cleanup (path, chan) =
-    Out_channel.close chan ;
-    Sys.remove path
+  let init () =
+    let p,ch = Filename.open_temp_file "lin-dsl-" "" in
+    path := p;
+    ch
 
-  (* turn [f: Out_channel.t -> ...] into [lift f: t -> ...] *)
-  let lift f (_, chan) = f chan
+  let cleanup chan =
+    Out_channel.close chan;
+    Sys.remove !path
 
   open Lin
   let int,int64 = nat_small,nat64_small
@@ -99,21 +101,21 @@ module OCConf : Lin.Spec = struct
     (* val_ "Out_channel.with_open_text"   Out_channel.with_open_text   (string @-> (t @-> 'a) @-> returning 'a) ; *)
     (* val_ "Out_channel.with_open_gen"    Out_channel.with_open_gen    (open_flag list @-> int @-> string @-> (t @-> 'a) @-> returning 'a) ; *)
 
-    val_ "Out_channel.seek"             (lift Out_channel.seek)             (t @-> int64 @-> returning_or_exc unit) ;
-    val_freq 3 "Out_channel.pos"        (lift Out_channel.pos)              (t @-> returning_or_exc int64) ;
-    val_freq 3 "Out_channel.length"     (lift Out_channel.length)           (t @-> returning_or_exc int64) ;
-    val_ "Out_channel.close"            (lift Out_channel.close)            (t @-> returning_or_exc unit) ;
-    val_ "Out_channel.close_noerr"      (lift Out_channel.close_noerr)      (t @-> returning unit) ;
-    val_ "Out_channel.flush"            (lift Out_channel.flush)            (t @-> returning_or_exc unit) ;
-    val_ "Out_channel.flush_all"              Out_channel.flush_all         (unit @-> returning_or_exc unit) ;
-    val_ "Out_channel.output_char"      (lift Out_channel.output_char)      (t @-> char @-> returning_or_exc unit) ;
-    val_ "Out_channel.output_byte"      (lift Out_channel.output_byte)      (t @-> int @-> returning_or_exc unit) ;
-    val_ "Out_channel.output_string"    (lift Out_channel.output_string)    (t @-> string @-> returning_or_exc unit) ;
-    val_ "Out_channel.output_bytes"     (lift Out_channel.output_bytes)     (t @-> bytes @-> returning_or_exc unit) ;
-    val_ "Out_channel.output"           (lift Out_channel.output)           (t @-> bytes @-> int @-> int @-> returning_or_exc unit) ;
-    val_ "Out_channel.output_substring" (lift Out_channel.output_substring) (t @-> string @-> int @-> int @-> returning_or_exc unit) ;
-    val_ "Out_channel.set_binary_mode"  (lift Out_channel.set_binary_mode)  (t @-> bool @-> returning_or_exc unit) ;
-    val_ "Out_channel.set_buffered"     (lift Out_channel.set_buffered)     (t @-> bool @-> returning_or_exc unit) ;
-    val_ "Out_channel.is_buffered"      (lift Out_channel.is_buffered)      (t @-> returning_or_exc bool) ;
+    val_ "Out_channel.seek"             Out_channel.seek             (t @-> int64 @-> returning_or_exc unit) ;
+    val_freq 3 "Out_channel.pos"        Out_channel.pos              (t @-> returning_or_exc int64) ;
+    val_freq 3 "Out_channel.length"     Out_channel.length           (t @-> returning_or_exc int64) ;
+    val_ "Out_channel.close"            Out_channel.close            (t @-> returning_or_exc unit) ;
+    val_ "Out_channel.close_noerr"      Out_channel.close_noerr      (t @-> returning unit) ;
+    val_ "Out_channel.flush"            Out_channel.flush            (t @-> returning_or_exc unit) ;
+    val_ "Out_channel.flush_all"        Out_channel.flush_all        (unit @-> returning_or_exc unit) ;
+    val_ "Out_channel.output_char"      Out_channel.output_char      (t @-> char @-> returning_or_exc unit) ;
+    val_ "Out_channel.output_byte"      Out_channel.output_byte      (t @-> int @-> returning_or_exc unit) ;
+    val_ "Out_channel.output_string"    Out_channel.output_string    (t @-> string @-> returning_or_exc unit) ;
+    val_ "Out_channel.output_bytes"     Out_channel.output_bytes     (t @-> bytes @-> returning_or_exc unit) ;
+    val_ "Out_channel.output"           Out_channel.output           (t @-> bytes @-> int @-> int @-> returning_or_exc unit) ;
+    val_ "Out_channel.output_substring" Out_channel.output_substring (t @-> string @-> int @-> int @-> returning_or_exc unit) ;
+    val_ "Out_channel.set_binary_mode"  Out_channel.set_binary_mode  (t @-> bool @-> returning_or_exc unit) ;
+    val_ "Out_channel.set_buffered"     Out_channel.set_buffered     (t @-> bool @-> returning_or_exc unit) ;
+    val_ "Out_channel.is_buffered"      Out_channel.is_buffered      (t @-> returning_or_exc bool) ;
   ]
 end
