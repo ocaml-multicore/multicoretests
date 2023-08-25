608f0d3e6ffd3248c2d7d98364d6dc87ef448880
Jan Midtgaard <mail@janmidtgaard.dk>
2023-08-25 13:49:23 +0200

    REMOVE ME: only run Out_channel test

--- src/io/lin_tests_dsl_domain.ml
+++ src/io/lin_tests_dsl_domain.ml
@@ -6,12 +6,12 @@ module IC_domain = Lin_domain.Make(Lin_tests_spec_io.ICConf)
 module OC_domain = Lin_domain.Make(Lin_tests_spec_io.OCConf)
 
 let tests =
-  IC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL In_channel test with Domain" ::
+(*  IC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL In_channel test with Domain" ::
   if Sys.getenv_opt "OCAML_SYSTEM" = Some "macosx"
   then (
     Printf.printf "Lin DSL Out_channel test with Domain disabled under macOS\n\n%!";
     []
-  ) else [
+    ) else*) [
     OC_domain.neg_lin_test ~count:5000 ~name:"Lin DSL Out_channel test with Domain";
   ]
 
