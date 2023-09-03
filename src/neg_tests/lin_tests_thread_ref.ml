2ecec962a9703188d4b197c96a0215da42795084
Jan Midtgaard <mail@janmidtgaard.dk>
2023-09-03 22:58:14 +0200

    Disable cmd shrinking for int64 ref Thread test

--- src/neg_tests/lin_tests_thread_ref.ml
+++ src/neg_tests/lin_tests_thread_ref.ml
@@ -3,7 +3,10 @@ open Lin_tests_common
 (** This is a driver of the negative ref tests over the Thread module *)
 
 module RT_int_thread = Lin_thread.Make_internal(RConf_int) [@alert "-internal"]
-module RT_int64_thread = Lin_thread.Make_internal(RConf_int64) [@alert "-internal"]
+module RT_int64_thread = Lin_thread.Make_internal(struct
+    include RConf_int64
+    let shrink_cmd = QCheck.Shrink.nil
+  end ) [@alert "-internal"]
 
 ;;
 if Sys.backend_type = Sys.Bytecode
