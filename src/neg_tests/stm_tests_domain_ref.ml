1633517948aa6373744882a97a68d6edcc963f5f
Jan Midtgaard <mail@janmidtgaard.dk>
2023-10-13 14:33:33 +0200

    try src/neg_tests/stm_tests_domain_ref.ml instead

--- src/neg_tests/stm_tests_domain_ref.ml
+++ src/neg_tests/stm_tests_domain_ref.ml
@@ -4,6 +4,6 @@ module RT_int   = STM_domain.Make(RConf_int)
 module RT_int64 = STM_domain.Make(RConf_int64)
 ;;
 QCheck_base_runner.run_tests_main
-  [RT_int.neg_agree_test_par        ~count:1000 ~name:"STM int ref test parallel";
-   RT_int64.neg_agree_test_par      ~count:1000 ~name:"STM int64 ref test parallel";
+  [RT_int.(*neg_*)agree_test_par        ~count:1 ~name:"STM int ref test parallel";
+   (*RT_int64.neg_agree_test_par      ~count:1000 ~name:"STM int64 ref test parallel";*)
   ]
