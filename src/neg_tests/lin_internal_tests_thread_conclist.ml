1cac4e9c374346184b1e6a9d4f98c5abab2a3378
Jan Midtgaard <mail@janmidtgaard.dk>
2024-01-23 09:24:38 +0100

    Disable int test too

--- src/neg_tests/lin_internal_tests_thread_conclist.ml
+++ src/neg_tests/lin_internal_tests_thread_conclist.ml
@@ -6,15 +6,11 @@ module CLT_int_thread = Lin_thread.Make_internal(CLConf (Int)) [@alert "-interna
 module CLT_int64_thread = Lin_thread.Make_internal(CLConf (Int64)) [@alert "-internal"]
 
 let _ =
-  let count = 1000 in
-  let tests = [
-    CLT_int_thread.lin_test   ~count ~name:"Lin.Internal CList int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
-    CLT_int64_thread.lin_test ~count ~name:"Lin.Internal CList int64 test with Thread" (* not triggering context switch, unfortunately *)
-  ] in
-  let tests =
-    if Sys.backend_type = Sys.Bytecode then (
-      Printf.printf "Lin.Internal CList int64 test with Thread disabled under bytecode\n\n%!";
-      [ List.hd tests ])
-    else tests
-  in
-  QCheck_base_runner.run_tests_main tests
+  if Sys.backend_type = Sys.Bytecode then
+    Printf.printf "Lin.Internal CList tests with Thread disabled under bytecode\n\n%!"
+  else
+    let count = 1000 in
+    QCheck_base_runner.run_tests_main [
+      CLT_int_thread.lin_test   ~count ~name:"Lin.Internal CList int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
+      CLT_int64_thread.lin_test ~count ~name:"Lin.Internal CList int64 test with Thread" (* not triggering context switch, unfortunately *)
+    ]
