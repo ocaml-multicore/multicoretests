cd7ecf9c711cb46df348a23d83e42d5a2ce85cdf
Jan Midtgaard <mail@janmidtgaard.dk>
2024-03-08 17:57:03 +0100

    Initial version of stress_{prop,test}

--- lib/lin_domain.ml
+++ lib/lin_domain.ml
@@ -29,11 +29,28 @@ module Make_internal (Spec : Internal.CmdSpec [@alert "-internal"]) = struct
               (fun (c,r) -> Printf.sprintf "%s : %s" (Spec.show_cmd c) (Spec.show_res r))
               (pref_obs,obs1,obs2)
 
+  (* "Don't crash under parallel usage" property *)
+  let stress_prop (seq_pref,cmds1,cmds2) =
+    let sut = Spec.init () in
+    let _pref_obs = interp sut seq_pref in
+    let wait = Atomic.make true in
+    let dom1 = Domain.spawn (fun () -> while Atomic.get wait do Domain.cpu_relax() done; try Ok (interp sut cmds1) with exn -> Error exn) in
+    let dom2 = Domain.spawn (fun () -> Atomic.set wait false; try Ok (interp sut cmds2) with exn -> Error exn) in
+    let obs1 = Domain.join dom1 in
+    let obs2 = Domain.join dom2 in
+    Spec.cleanup sut ;
+    let _obs1 = match obs1 with Ok v -> v | Error exn -> raise exn in
+    let _obs2 = match obs2 with Ok v -> v | Error exn -> raise exn in
+    true
+
   let lin_test ~count ~name =
-    lin_test ~rep_count:50 ~count ~retries:3 ~name ~lin_prop:lin_prop
+    M.lin_test ~rep_count:50 ~count ~retries:3 ~name ~lin_prop:lin_prop
 
   let neg_lin_test ~count ~name =
     neg_lin_test ~rep_count:50 ~count ~retries:3 ~name ~lin_prop:lin_prop
+
+  let stress_test ~count ~name =
+    M.lin_test ~rep_count:50 ~count ~retries:3 ~name ~lin_prop:stress_prop
 end
 
 module Make (Spec : Spec) = Make_internal(MakeCmd(Spec))
