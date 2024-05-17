adff14c5ec099a22a0fbe363797e14d97b700c26
Jan Midtgaard <mail@janmidtgaard.dk>
2024-05-17 11:32:18 +0200

    Bump STM Sys test parallel count to 2500

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -330,5 +330,5 @@ QCheck_base_runner.run_tests_main [
     Sys_seq.agree_test              ~count:1000 ~name:"STM Sys test sequential";
     if Sys.unix && uname_os () = Some "Linux"
     then Sys_dom.agree_test_par     ~count:200  ~name:"STM Sys test parallel"
-    else Sys_dom.neg_agree_test_par ~count:1000 ~name:"STM Sys test parallel"
+    else Sys_dom.neg_agree_test_par ~count:2500 ~name:"STM Sys test parallel"
   ]
