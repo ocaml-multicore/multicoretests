5872ebc359f8c0d963c060556a220ab58977376f
Jan Midtgaard <mail@janmidtgaard.dk>
2024-06-11 21:12:17 +0200

    Add STM Sys stress test

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -330,5 +330,6 @@ QCheck_base_runner.run_tests_main [
     Sys_seq.agree_test              ~count:1000 ~name:"STM Sys test sequential";
     if Sys.unix && uname_os () = Some "Linux"
     then Sys_dom.agree_test_par     ~count:200  ~name:"STM Sys test parallel"
-    else Sys_dom.neg_agree_test_par ~count:2500 ~name:"STM Sys test parallel"
+    else Sys_dom.neg_agree_test_par ~count:2500 ~name:"STM Sys test parallel";
+    Sys_dom.stress_test_par         ~count:1000 ~name:"STM Sys stress test parallel";
   ]
