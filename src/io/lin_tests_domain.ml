dd58f8ce7bb01609fa1866339a59a5c5569568c2
Jan Midtgaard <mail@janmidtgaard.dk>
2024-08-12 13:35:34 +0200

    Only run Lin Out_channel test

--- src/io/lin_tests_domain.ml
+++ src/io/lin_tests_domain.ml
@@ -6,13 +6,13 @@ module IC_domain = Lin_domain.Make(Lin_tests_spec_io.ICConf)
 module OC_domain = Lin_domain.Make(Lin_tests_spec_io.OCConf)
 
 let tests =
-  IC_domain.neg_lin_test ~count:1000 ~name:"Lin In_channel test with Domain" ::
+(*IC_domain.neg_lin_test ~count:1000 ~name:"Lin In_channel test with Domain" ::
   IC_domain.stress_test  ~count:1000 ~name:"Lin In_channel stress test with Domain" ::
   if Sys.getenv_opt "OCAML_SYSTEM" = Some "macosx"
   then (
     Printf.printf "Lin Out_channel test with Domain disabled under macOS\n\n%!";
     []
-  ) else [
+  ) else*) [
     OC_domain.neg_lin_test ~count:5000 ~name:"Lin Out_channel test with Domain";
   ]
 
