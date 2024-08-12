925254a5b77289f99d26a609c54a787b503aab71
Jan Midtgaard <mail@janmidtgaard.dk>
2024-08-12 13:32:13 +0200

    Revert "Remove Lin Out_channel tests"
    This reverts commit bc1bee404e42c8b77d307b9d1239ccbcd0bc5eed.

--- src/io/lin_tests_domain.ml
+++ src/io/lin_tests_domain.ml
@@ -1,11 +1,19 @@
 (* ********************************************************************** *)
-(*                      Tests of In_channels                              *)
+(*                      Tests of in and out channels                      *)
 (* ********************************************************************** *)
 
 module IC_domain = Lin_domain.Make(Lin_tests_spec_io.ICConf)
+module OC_domain = Lin_domain.Make(Lin_tests_spec_io.OCConf)
 
-let _ =
-  QCheck_base_runner.run_tests_main [
-    IC_domain.neg_lin_test ~count:1000 ~name:"Lin In_channel test with Domain";
-    IC_domain.stress_test  ~count:1000 ~name:"Lin In_channel stress test with Domain";
+let tests =
+  IC_domain.neg_lin_test ~count:1000 ~name:"Lin In_channel test with Domain" ::
+  IC_domain.stress_test  ~count:1000 ~name:"Lin In_channel stress test with Domain" ::
+  if Sys.getenv_opt "OCAML_SYSTEM" = Some "macosx"
+  then (
+    Printf.printf "Lin Out_channel test with Domain disabled under macOS\n\n%!";
+    []
+  ) else [
+    OC_domain.neg_lin_test ~count:5000 ~name:"Lin Out_channel test with Domain";
   ]
+
+let _ = QCheck_base_runner.run_tests_main tests
--- src/io/lin_tests_thread.ml
+++ src/io/lin_tests_thread.ml
@@ -1,10 +1,12 @@
 (* ********************************************************************** *)
-(*                      Tests of In_channels                              *)
+(*                      Tests of in and out channels                      *)
 (* ********************************************************************** *)
 
 module IC_thread = Lin_thread.Make(Lin_tests_spec_io.ICConf) [@@alert "-experimental"]
+module OC_thread = Lin_thread.Make(Lin_tests_spec_io.OCConf) [@@alert "-experimental"]
 
 let _ =
   QCheck_base_runner.run_tests_main [
     IC_thread.neg_lin_test ~count:1000 ~name:"Lin In_channel test with Thread";
+    OC_thread.neg_lin_test ~count:1000 ~name:"Lin Out_channel test with Thread";
   ]
