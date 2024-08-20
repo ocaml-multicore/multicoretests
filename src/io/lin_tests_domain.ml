1dd70b342dfb33fd91e247a5dc82c25da4ffbcea
Jan Midtgaard <mail@janmidtgaard.dk>
2024-08-20 16:51:20 +0200

    Omit stresstest to reproduce s->running assertion failure

--- src/io/lin_tests_domain.ml
+++ src/io/lin_tests_domain.ml
@@ -7,5 +7,5 @@ module IC_domain = Lin_domain.Make(Lin_tests_spec_io.ICConf)
 let _ =
   QCheck_base_runner.run_tests_main [
     IC_domain.neg_lin_test ~count:1000 ~name:"Lin In_channel test with Domain";
-    IC_domain.stress_test  ~count:1000 ~name:"Lin In_channel stress test with Domain";
+  (*IC_domain.stress_test  ~count:1000 ~name:"Lin In_channel stress test with Domain";*)
   ]
