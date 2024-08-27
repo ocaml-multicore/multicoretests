23495d9d213f8d73eec04db247a439c595c97104
Jan Midtgaard <mail@janmidtgaard.dk>
2024-08-27 16:49:18 +0200

    Add Gc.minor_words and allocated_bytes and a cmd to alloc a string

--- src/gc/stm_tests.ml
+++ src/gc/stm_tests.ml
@@ -7,50 +7,70 @@ module GCConf =
 struct
   type cmd =
     | Counters
+    | Minor_words
     | Minor
     | Full_major
     | Compact
+    | Allocated_bytes
     | Cons64 of int
+    | AllocStr of int * int
 
   let pp_cmd par fmt x =
     let open Util.Pp in
     match x with
-    | Counters   -> cst0 "Counters" fmt
-    | Minor      -> cst0 "Minor" fmt
-    | Full_major -> cst0 "Full_major" fmt
-    | Compact    -> cst0 "Compact" fmt
-    | Cons64 i   -> cst1 pp_int "Cons64" par fmt i
+    | Counters    -> cst0 "Counters" fmt
+    | Minor_words -> cst0 "Minor_words" fmt
+    | Minor       -> cst0 "Minor" fmt
+    | Full_major  -> cst0 "Full_major" fmt
+    | Compact     -> cst0 "Compact" fmt
+    | Allocated_bytes -> cst0 "Allocated_bytes" fmt
+    | Cons64 i    -> cst1 pp_int "Cons64" par fmt i
+    | AllocStr (i,l) -> cst2 pp_int pp_int "AllocStr" par fmt i l
 
   let show_cmd = Util.Pp.to_show pp_cmd
 
   type state = unit
-  type sut   = { mutable int64s : int64 list }
+  let init_state  = ()
+
+  let array_length = 8
 
   let arb_cmd _s =
     let int_gen = Gen.small_nat in
+    let len_gen = Gen.(map (fun shift -> 1 lsl (shift-1)) (int_bound 14)) in (*[-1;13] ~ [0;1;...4096;8196] *)
+    let index_gen = Gen.int_bound (array_length-1) in
     QCheck.make ~print:show_cmd
       Gen.(frequency
-             [ 1, return Counters;
+             [ 1, return Counters;  (* known problem with Counters on <= 5.2: https://github.com/ocaml/ocaml/pull/13370 *)
+               1, return Minor_words;
                1, return Minor;
                1, return Full_major;
                1, return Compact;
-               20, map (fun i -> Cons64 i) int_gen;
+               1, return Allocated_bytes;
+               10, map (fun i -> Cons64 i) int_gen;
+               10, map2 (fun index len -> AllocStr (index,len)) index_gen len_gen;
              ])
 
-  let init_state  = ()
-
   let next_state n _s = match n with
-    | Counters   -> ()
-    | Minor      -> ()
-    | Full_major -> ()
-    | Compact    -> ()
-    | Cons64 _   -> ()
+    | Counters    -> ()
+    | Minor_words -> ()
+    | Minor       -> ()
+    | Full_major  -> ()
+    | Compact     -> ()
+    | Allocated_bytes -> ()
+    | Cons64 _    -> ()
+    | AllocStr _   -> ()
 
-  let init_sut () = { int64s = [] }
+  type sut =
+    { mutable int64s  : int64 list;
+      mutable strings : string array; }
+  let init_sut () =
+    { int64s = [];
+      strings = Array.make array_length ""; }
 
   let cleanup sut =
     begin
       sut.int64s <- [];
+      sut.strings <- [| |];
       Gc.compact ()
     end
 
@@ -66,20 +86,26 @@ struct
     (Tup3 (ty_a,ty_b,ty_c), QCheck.Print.tup3 show_a show_b show_c)
 
   let run c sut = match c with
-    | Counters   -> Res (tup3 float float float, Gc.counters ())
-    | Minor      -> Res (unit, Gc.minor ())
-    | Full_major -> Res (unit, Gc.full_major ())
-    | Compact    -> Res (unit, Gc.compact ())
-    | Cons64 i   -> Res (unit, sut.int64s <- (Int64.of_int i)::sut.int64s) (*alloc int64 and cons cell at test runtime*)
+    | Counters    -> Res (tup3 float float float, Gc.counters ())
+    | Minor_words -> Res (float, Gc.minor_words ())
+    | Minor       -> Res (unit, Gc.minor ())
+    | Full_major  -> Res (unit, Gc.full_major ())
+    | Compact     -> Res (unit, Gc.compact ())
+    | Allocated_bytes -> Res (float, Gc.allocated_bytes ())
+    | Cons64 i    -> Res (unit, sut.int64s <- ((Int64.of_int i)::sut.int64s)) (*alloc int64 and cons cell at test runtime*)
+    | AllocStr (i,len) -> Res (unit, sut.strings.(i) <- (String.make len 'c')) (*alloc string at test runtime*)
 
   let postcond n (_s: unit) res = match n, res with
     | Counters, Res ((Tup3 (Float,Float,Float),_),r) ->
       let (minor_words, promoted_words, major_words) = r in
       minor_words >= 0. && promoted_words >= 0. && major_words >= 0.
+    | Minor_words, Res ((Float,_),r) -> r >= 0.
     | Minor,      Res ((Unit,_), ()) -> true
     | Full_major, Res ((Unit,_), ()) -> true
     | Compact,    Res ((Unit,_), ()) -> true
+    | Allocated_bytes, Res ((Float,_),r) -> r >= 0.
     | Cons64 _,   Res ((Unit,_), ()) -> true
+    | AllocStr _,  Res ((Unit,_), ()) -> true
     | _, _ -> false
 end
 
