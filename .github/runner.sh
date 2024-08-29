e40f424cc286f8e7eeb63a87fb7a75439fef8030
Samuel Hym <samuel@tarides.com>
2024-08-29 14:51:40 +0200

    Merge the CI workflows and stop using `setup-ocaml`
    Merge the two CI main workflows, namely `common.yml` and
    `msvc-common.yml` into a single `common.yml` workflow that uses neither
    the `setup-ocaml` action nor `OPAM` so that:
    
    - the workflow has reduced dependencies and a smaller footprint,
    - the compiler configuration can be tuned (to disable features that are
      not relevant to the multicoretests, such as manpages),
    - it gets easier to test any branch of any fork of the compiler,
    - it brings the workflow really close to a version that can be pushed to
      the compiler repository.
    
    The logic of the workflow is largely extracted to a `runner.sh` script
    so that:
    
    - it is easier to maintain, as it is a regular script, instead of being
      embedded into a different syntax, especially with the issue of
      portability to Windows due to line endings,
    - it is easy to reuse that logic for different setups than regular GH
      Action runners, such as VMs, other infrastructures, etc.
    
    Extra notes:
    
    - the actual test steps are currently not triggered through the
      `runner.sh` script, in the unlikely case this could make some
      difference on Windows to have an `sh` involved somewhere in the stack,
    - the new common-workflow default is to test `trunk` rather than 5.0.0,
      which seems a better default.

---
+++ .github/runner.sh
@@ -0,0 +1,208 @@
+#!/bin/sh
+
+set -e
+
+OCAMLDIR=ocaml
+DUNEDIR=dune
+
+fatal() {
+  printf %s "$1"
+  exit 1
+}
+
+compiler_sha() {
+  # Expect $COMPILER_REPO and $COMPILER_REF to be set
+  git ls-remote "https://github.com/$COMPILER_REPO.git" "$COMPILER_REF" \
+    | cut -f 1
+}
+
+setup() {
+  if [ -n "$GITHUB_ENV" ] && [ -n "$GITHUB_PATH" ] ; then
+    echo "pristine_path=$PATH" >> "$GITHUB_ENV"
+    sha="$(compiler_sha)" || fatal "Cannot find compiler's SHA"
+    arch="$(uname -m)"
+    opts="$(printf %s "$OCAML_OPTIONS" | tr " " -)"
+    opts="${opts:+-}$opts"
+    echo "cache_key=ocaml-$sha-$OCAML_PLATFORM-$arch$opts" >> "$GITHUB_ENV"
+    case "$OCAML_PLATFORM" in
+      mingw|msvc)
+        prefix='D:\ocaml'
+        bin='D:\ocaml\bin'
+        ;;
+      cygwin)
+        # On Cygwin, we want both directory syntaxes in PATH
+        prefix='/cygdrive/d/ocaml'
+        bin='/cygdrive/d/ocaml/bin'
+        printf 'D:\\ocaml\\bin\n' >> "$GITHUB_PATH"
+        ;;
+      *)
+        prefix="$HOME/local"
+        bin="$HOME/local/bin"
+        ;;
+    esac
+    printf "prefix=%s\n" "$prefix" >> "$GITHUB_ENV"
+    printf "%s\n" "$bin" >> "$GITHUB_PATH"
+    if [ -z "$JOBS" ] ; then
+      if command -v nproc > /dev/null; then
+        echo "JOBS=$(nproc)" >> "$GITHUB_ENV"
+      elif command -v sysctl > /dev/null; then
+        echo "JOBS=$(sysctl -n hw.ncpu)" >> "$GITHUB_ENV"
+      fi
+    fi
+
+    echo Environment set up:
+    cat "$GITHUB_ENV"
+    echo PATH addition:
+    cat "$GITHUB_PATH"
+  fi
+
+  case "$OCAML_PLATFORM,$OCAML_OPTIONS" in
+    linux,*32bit*)
+      sudo dpkg --add-architecture i386
+      sudo apt-get update
+      sudo apt-get install pkg-config:i386 libzstd1:i386 libzstd-dev:i386 \
+        libgcc-s1:i386 gcc-multilib g++-multilib
+      ;;
+  esac
+}
+
+build_ocaml() {
+  echo "${LOGBEGINGRP}Building OCaml"
+  # We let standard OCaml CI test for warnings
+  opts="--disable-warn-error \
+        --disable-stdlib-manpages \
+        --disable-ocamltest \
+        --disable-ocamldoc"
+  case "$OCAML_OPTIONS" in
+    *fp*)
+      opts="$opts --enable-frame-pointers"
+      ;;
+  esac
+  case "$OCAML_OPTIONS" in
+    *bytecode-only*)
+      opts="$opts --disable-native-compiler"
+      ;;
+  esac
+  case "$OCAML_PLATFORM" in
+    msvc|mingw|cygwin)
+      opts="$opts --prefix=/cygdrive/d/ocaml"
+      ;;
+    *)
+      opts="$opts --prefix=$prefix"
+      ;;
+  esac
+
+  cd "$OCAMLDIR"
+  case "$OCAML_PLATFORM,$OCAML_OPTIONS" in
+    msvc,*32bit*)
+      eval $(tools/msvs-promote-path)
+      printf 'Running: %s\n' "./configure --host=i686-pc-windows $opts"
+      if ! ./configure --host=i686-pc-windows $opts ; then
+        cat config.log
+        exit 1
+      fi
+      ;;
+    msvc,*)
+      eval $(tools/msvs-promote-path)
+      printf 'Running: %s\n' "./configure --host=x86_64-pc-windows $opts"
+      if ! ./configure --host=x86_64-pc-windows $opts ; then
+        cat config.log
+        exit 1
+      fi
+      ;;
+    mingw,*)
+      printf 'Running: %s\n' "./configure --host=x86_64-w64-mingw32 $opts"
+      if ! ./configure --host=x86_64-w64-mingw32 $opts ; then
+        cat config.log
+        exit 1
+      fi
+      ;;
+    cygwin,*)
+      case $COMPILER_REF in
+        */5.1*)
+          git -C flexdll fetch origin 0.43
+          git -C flexdll checkout FETCH_HEAD
+          ;;
+      esac
+
+      printf 'Running: %s\n' "./configure $opts"
+      if ! ./configure $opts ; then
+        cat config.log
+        exit 1
+      fi
+      ;;
+    linux,*32bit*)
+      printf 'Running: %s\n' \
+        "./configure --host=i386-linux \"CC=gcc -m32\" $opts"
+      if ! ./configure --host=i386-linux "CC=gcc -m32" $opts ; then
+        cat config.log
+        exit 1
+      fi
+      ;;
+    *) # linux, macos, default options
+      printf 'Running: %s\n' "./configure $opts"
+      if ! ./configure $opts ; then
+        cat config.log
+        exit 1
+      fi
+      ;;
+  esac
+
+  make -j"$JOBS"
+  make install
+
+  echo "$LOGENDGRP"
+}
+
+build_dune() {
+  echo "${LOGBEGINGRP}Building dune"
+  case "$OCAML_PLATFORM" in
+    msvc)
+      eval $("$OCAMLDIR"/tools/msvs-promote-path)
+      ;;
+  esac
+
+  cd "$DUNEDIR"
+  make release
+  make install PREFIX="$prefix"
+  echo "$LOGENDGRP"
+}
+
+show_config() {
+  set -x
+  ocamlc -config
+  dune --version
+}
+
+build_testsuite() {
+  case "$OCAML_PLATFORM" in
+    msvc)
+      eval $("$OCAMLDIR"/tools/msvs-promote-path)
+      ;;
+  esac
+
+  cd multicoretests
+  dune build
+  dune build test/
+}
+
+case "$1" in
+  setup)
+    setup
+    ;;
+  ocaml)
+    build_ocaml
+    ;;
+  dune)
+    build_dune
+    ;;
+  show_config)
+    show_config
+    ;;
+  build)
+    build_testsuite
+    ;;
+  *)
+    fatal "Unknown command '$1'"
+    ;;
+esac
