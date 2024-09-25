fd0a3fd4cc23c7ea0df84b1d45460d113fb97595
Jan Midtgaard <mail@janmidtgaard.dk>
2024-09-25 15:47:10 +0200

    Adjust minor_heap_size test to be pagesize-aware

---
+++ src/gc/pagesizestub.c
@@ -0,0 +1,25 @@
+#ifdef _WIN32
+#include <sysinfoapi.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "caml/mlvalues.h"
+#include "caml/memory.h"
+
+CAMLprim value page_size(value ignored) {
+  CAMLparam1(ignored);
+  CAMLlocal1(result);
+
+  long ps;
+#ifdef _WIN32
+  SYSTEM_INFO si;
+  GetSystemInfo(&si);
+  ps = si.dwPageSize;
+#else
+  ps = sysconf(_SC_PAGESIZE); // page size in bytes
+#endif
+
+  result = Val_int(ps);
+  CAMLreturn(result);
+}
