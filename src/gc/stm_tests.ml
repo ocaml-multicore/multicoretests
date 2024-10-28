167148797385fcb87b5465e6304d7cd509729658
Jan Midtgaard <mail@janmidtgaard.dk>
2024-10-28 11:10:29 +0100

    Run only stress test

--- src/gc/stm_tests.ml
+++ src/gc/stm_tests.ml
@@ -2,7 +2,7 @@
 
 (* sequential and parallel tests of the GC with explicit Gc invocations *)
 
-module GC_STM_seq = STM_sequential.Make(GCConf)
+(*module GC_STM_seq = STM_sequential.Make(GCConf)*)
 module GC_STM_dom = STM_domain.Make(GCConf)
 (*
 let agree_prop cs = match Util.protect GC_STM_seq.agree_prop cs with
@@ -26,7 +26,7 @@ let _ =
   Printf.printf "Page size: %i\n" (Pagesize.get ());
   QCheck_base_runner.run_tests_main [
 (*  agree_test                    ~count:1000 ~name:"STM Gc test sequential";
-    agree_child_test              ~count:1000 ~name:"STM Gc test sequential in child domain";*)
-    GC_STM_dom.neg_agree_test_par ~count:1000 ~name:"STM Gc test parallel";
+    agree_child_test              ~count:1000 ~name:"STM Gc test sequential in child domain";
+    GC_STM_dom.neg_agree_test_par ~count:1000 ~name:"STM Gc test parallel";*)
     GC_STM_dom.stress_test_par    ~count:1000 ~name:"STM Gc stress test parallel";
   ]
