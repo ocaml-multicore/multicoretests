7a629e38fa91b5d67c58b92a5c6e488e2df4b64d
Jan Midtgaard <mail@janmidtgaard.dk>
2024-12-11 13:25:07 +0100

    Split src/gc/stm_tests_impl.ml in three, to start each from a clean-state heap

---
+++ src/gc/stm_tests_impl_par.ml
@@ -0,0 +1,15 @@
+(* parallel tests of the GC, without explicit Gc invocations *)
+
+module ImplGCConf =
+struct
+  include GCConf
+  let arb_cmd = arb_alloc_cmd
+end
+
+module GC_STM_dom = STM_domain.Make(ImplGCConf)
+
+let _ =
+  Printf.printf "Page size: %i\n" (Pagesize.get ());
+  QCheck_base_runner.run_tests_main [
+    GC_STM_dom.agree_test_par ~count:1000 ~name:"STM implicit Gc test parallel";
+  ]
