ca1031d827739d2638a12da879c73f8c0644bbec
Jan Midtgaard <mail@janmidtgaard.dk>
2024-12-11 13:25:07 +0100

    Split src/gc/stm_tests_impl.ml in three, to start each from a clean-state heap

---
+++ src/gc/stm_tests_impl_seq.ml
@@ -0,0 +1,25 @@
+open QCheck
+
+(* sequential tests of the GC, without explicit Gc invocations *)
+
+module ImplGCConf =
+struct
+  include GCConf
+  let arb_cmd = arb_alloc_cmd
+end
+
+module GC_STM_seq = STM_sequential.Make(ImplGCConf)
+
+let agree_prop cs = match Util.protect GC_STM_seq.agree_prop cs with
+  | Ok r -> r
+  | Error Stack_overflow -> true (* Stack_overflow is accepted behaviour *)
+  | Error e -> raise e
+
+let agree_test ~count ~name =
+  Test.make ~name ~count (GC_STM_seq.arb_cmds ImplGCConf.init_state) agree_prop
+
+let _ =
+  Printf.printf "Page size: %i\n" (Pagesize.get ());
+  QCheck_base_runner.run_tests_main [
+    agree_test ~count:1000 ~name:"STM implicit Gc test sequential";
+  ]
