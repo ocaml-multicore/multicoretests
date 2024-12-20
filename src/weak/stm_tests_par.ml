51efb808f6f33ddc4a661fcec13251bde8d9d238
Jan Midtgaard <mail@janmidtgaard.dk>
2024-12-20 11:53:17 +0100

    Factor Weak STM tests into separate executable tests to reduce GC interference

---
+++ src/weak/stm_tests_par.ml
@@ -0,0 +1,7 @@
+(** parallel STM tests of Weak arrays *)
+
+module WeakSTM_dom = STM_domain.Make(Stm_tests_spec_weak)
+
+let _ =
+  QCheck_base_runner.run_tests_main
+    [ WeakSTM_dom.neg_agree_test_par ~count:5000 ~name:"STM Weak test parallel" ]
