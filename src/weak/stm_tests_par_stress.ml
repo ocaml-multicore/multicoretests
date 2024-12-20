64716fdb059f36de7fa9d3b64ded92a23ec400fd
Jan Midtgaard <mail@janmidtgaard.dk>
2024-12-20 12:18:47 +0100

    Rename stm_tests_spec_weak -> src/weak/stm_tests_weak_spec

--- src/weak/stm_tests_par_stress.ml
+++ src/weak/stm_tests_par_stress.ml
@@ -1,6 +1,6 @@
 (** parallel stress STM tests of Weak arrays *)
 
-module WeakSTM_dom = STM_domain.Make(Stm_tests_spec_weak)
+module WeakSTM_dom = STM_domain.Make(Stm_tests_weak_spec)
 
 let _ =
   QCheck_base_runner.run_tests_main
--- src/weak/stm_tests_seq.ml
+++ src/weak/stm_tests_seq.ml
@@ -1,6 +1,6 @@
 (** sequential STM tests of Weak arrays *)
 
-module WeakSTM_seq = STM_sequential.Make(Stm_tests_spec_weak)
+module WeakSTM_seq = STM_sequential.Make(Stm_tests_weak_spec)
 
 let _ =
   QCheck_base_runner.run_tests_main
