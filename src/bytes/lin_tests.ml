1794710c616b9dbe3e6b4b33e06bc591ad902f22
Jan Midtgaard <mail@janmidtgaard.dk>
2025-01-16 14:11:45 +0100

    Move Bytes.length first in Lin Bytes to agree with interface ordering

--- src/bytes/lin_tests.ml
+++ src/bytes/lin_tests.ml
@@ -9,10 +9,10 @@ module BConf = struct
   open Lin
   let int,string = nat_small, string_small_printable
   let api = [
+    val_ "Bytes.length"      Bytes.length      (t @-> returning int);
     val_ "Bytes.get"         Bytes.get         (t @-> int @-> returning_or_exc char);
     val_ "Bytes.set"         Bytes.set         (t @-> int @-> char @-> returning_or_exc unit);
     val_ "Bytes.sub_string"  Bytes.sub_string  (t @-> int @-> int @-> returning_or_exc string);
-    val_ "Bytes.length"      Bytes.length      (t @-> returning int);
     val_ "Bytes.fill"        Bytes.fill        (t @-> int @-> int @-> char @-> returning_or_exc unit);
     val_ "Bytes.blit_string" Bytes.blit_string (string @-> int @-> t @-> int @-> int @-> returning_or_exc unit);
     val_ "Bytes.index_from"  Bytes.index_from  (t @-> int @-> char @-> returning_or_exc int)]
