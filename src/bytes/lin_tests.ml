0cada98cae22ca439d0eb768d63e4cde8f72bd05
Jan Midtgaard <mail@janmidtgaard.dk>
2025-01-18 15:32:58 +0100

    Add Lin Bytes stress test

--- src/bytes/lin_tests.ml
+++ src/bytes/lin_tests.ml
@@ -47,4 +47,5 @@ module BT_thread = Lin_thread.Make(BConf) [@alert "-experimental"]
 QCheck_base_runner.run_tests_main [
   BT_domain.neg_lin_test ~count:2500 ~name:"Lin Bytes test with Domain";
   BT_thread.lin_test     ~count:250  ~name:"Lin Bytes test with Thread";
+  BT_domain.stress_test  ~count:1000 ~name:"Lin Bytes stress test with Domain";
 ]
