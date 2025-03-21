c79372bd0a3f5d36bf0e4ca68bb49e61b92a17bb
Jan Midtgaard <mail@janmidtgaard.dk>
2025-03-21 12:37:44 +0100

    Toggle {Lin,STM}.thread int64 ref difference under bytecode

--- src/neg_tests/lin_tests_thread.ml
+++ src/neg_tests/lin_tests_thread.ml
@@ -8,10 +8,16 @@ module RT_int64_thread = Lin_thread.Make(Ref_int64_spec)
 module CLT_int_thread = Lin_thread.Make(CList_spec_int(CList_bis))
 module CLT_int64_thread = Lin_thread.Make(CList_spec_int64(CList_bis))
 
+let count = 1000
+let rt_int64_lin_thread_test =
+  if Sys.backend_type = Sys.Bytecode (* test is considered positive under bytecode *)
+  then
+    RT_int64_thread.lin_test ~count ~name:"Lin ref int64 test with Thread"
+  else
+    RT_int64_thread.neg_lin_test  ~count:15000 ~name:"Lin ref int64 test with Thread";
 ;;
 QCheck_base_runner.run_tests_main
-  (let count = 1000 in
-   [RT_int_thread.lin_test        ~count ~name:"Lin ref int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
-    RT_int64_thread.neg_lin_test  ~count:15000 ~name:"Lin ref int64 test with Thread";
-    CLT_int_thread.neg_lin_test   ~count ~name:"Lin CList int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
-    CLT_int64_thread.neg_lin_test ~count ~name:"Lin CList int64 test with Thread"]) (* not triggering context switch, unfortunately *)
+  ([RT_int_thread.lin_test        ~count ~name:"Lin ref int test with Thread"; (* unboxed, hence no allocations to trigger context switch *)
+    rt_int64_lin_thread_test;
+    CLT_int_thread.neg_lin_test   ~count ~name:"Lin CList int test with Thread";
+    CLT_int64_thread.neg_lin_test ~count ~name:"Lin CList int64 test with Thread"])
--- src/neg_tests/stm_tests_thread_ref.ml
+++ src/neg_tests/stm_tests_thread_ref.ml
@@ -3,11 +3,11 @@ open Stm_tests_spec_ref
 module RT_int   = STM_thread.Make(RConf_int)
 module RT_int64 = STM_thread.Make(RConf_int64)
 ;;
-if Sys.backend_type = Sys.Bytecode
-then
-  Printf.printf "STM ref tests with Thread disabled under bytecode\n\n%!"
-else
 QCheck_base_runner.run_tests_main
   [RT_int.agree_test_conc       ~count:250  ~name:"STM int ref test with Thread";
-   RT_int64.neg_agree_test_conc ~count:5000 ~name:"STM int64 ref test with Thread";
+   if Sys.backend_type = Sys.Bytecode (* test is considered positive under bytecode *)
+   then
+     RT_int64.agree_test_conc     ~count:1000 ~name:"STM int64 ref test with Thread"
+   else
+     RT_int64.neg_agree_test_conc ~count:5000 ~name:"STM int64 ref test with Thread";
   ]
