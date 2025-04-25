e759b2a0fb7d1b8256e64a6e7189d2da756dfa71
Jan Midtgaard <mail@janmidtgaard.dk>
2025-04-26 00:11:12 +0200

    Clean up commented out code

--- src/gc/stm_tests_spec.ml
+++ src/gc/stm_tests_spec.ml
@@ -6,7 +6,6 @@ type cmd =
   | Compact
   (* cmds to allocate memory *)
   | PreAllocList of int * char list
-(*| AllocList of int * int*)
   | RevList of int
 
 let pp_cmd par fmt x =
@@ -15,7 +14,6 @@ let pp_cmd par fmt x =
   | Set_minor_heap_size i -> cst1 pp_int "Set minor_heap_size" par fmt i
   | Compact     -> cst0 "Compact" fmt
   | PreAllocList (i,l) -> cst2 pp_int (pp_list pp_char) "PreAllocList" par fmt i l
-(*| AllocList (i,l) -> cst2 pp_int pp_int "AllocList" par fmt i l*)
   | RevList i   -> cst1 pp_int "RevList" par fmt i
 
 let show_cmd = Util.Pp.to_show pp_cmd
@@ -36,7 +34,6 @@ let alloc_cmds, gc_cmds =
     Gen.([
         (* allocating cmds to activate the Gc *)
         5, map2 (fun index list -> PreAllocList (index,list)) index_gen list_gen;
-      (*5, map2 (fun index len -> AllocList (index,len)) index_gen Gen.nat;*)
         5, map (fun index -> RevList index) index_gen;
       ]) in
   let gc_cmds =
@@ -69,13 +66,11 @@ let run c sut = match c with
   | Set_minor_heap_size i -> Res (unit, Gc.set { orig_control with minor_heap_size = i; })
   | Compact     -> Res (unit, Gc.compact ())
   | PreAllocList (i,l) -> Res (unit, sut.lists.(i) <- l) (*alloc list in parent domain in test-input*)
-(*| AllocList (i,len) -> Res (unit, sut.lists.(i) <- List.init len (fun _ -> 'a')) (*alloc list at test runtime*)*)
   | RevList i -> Res (unit, sut.lists.(i) <- List.rev sut.lists.(i)) (*alloc list at test runtime*)
 
 let postcond n (_s: state) res = match n, res with
   | Set_minor_heap_size _, Res ((Unit,_), ()) -> true
   | Compact,    Res ((Unit,_), ()) -> true
   | PreAllocList _, Res ((Unit,_), ()) -> true
-(*| AllocList _, Res ((Unit,_), ()) -> true*)
   | RevList _,  Res ((Unit,_), ()) -> true
   | _, _ -> false
