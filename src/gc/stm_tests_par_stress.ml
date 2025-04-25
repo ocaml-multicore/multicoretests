873f2d8aafbdcfda889d38e9cf85554b09f61318
Jan Midtgaard <mail@janmidtgaard.dk>
2025-04-25 19:38:01 +0200

    Experiment: halve count, double repetition count

--- src/gc/stm_tests_par_stress.ml
+++ src/gc/stm_tests_par_stress.ml
@@ -5,5 +5,5 @@ module GC_STM_dom = STM_domain.Make(Stm_tests_spec)
 let _ =
   Printf.printf "Page size: %i\n" (Pagesize.get ());
   QCheck_base_runner.run_tests_main [
-    GC_STM_dom.stress_test_par    ~count:1000 ~name:"STM Gc stress test parallel";
+    GC_STM_dom.stress_test_par    ~count:500 ~name:"STM Gc stress test parallel";
   ]
