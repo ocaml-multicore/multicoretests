43ab428c651e69bb755dce87ddd809623df44528
Jan Midtgaard <mail@janmidtgaard.dk>
2025-04-28 18:29:10 +0200

    Eliminate STM.domain

--- src/gc/stm_tests_par_stress.ml
+++ src/gc/stm_tests_par_stress.ml
@@ -18,7 +18,6 @@ struct
     | PreAllocList (i,_l) -> "PreAllocList " ^ (string_of_int i)
     | RevList i   -> "RevList " ^ (string_of_int i)
 
-  type state = unit
   let init_state = ()
 
   let array_length = 4
@@ -37,7 +36,6 @@ struct
 
   let next_state _n _s = ()
 
-  type sut = unit list array
   let init_sut () = Array.make array_length []
 
   let orig_control = Gc.get ()
@@ -51,20 +49,84 @@ struct
       Gc.major ()
     end
 
-  let precond _n _s = true
-
   let run c sut = match c with
     | Set_minor_heap_size_2048 -> Res (unit, Gc.set { orig_control with minor_heap_size = 2048; })
     | Compact     -> Res (unit, Gc.compact ())
     | PreAllocList (i,l) -> Res (unit, sut.(i) <- l) (*alloc list in parent domain in test-input*)
     | RevList i -> Res (unit, sut.(i) <- List.rev sut.(i)) (*alloc list at test runtime*)
-
-  let postcond _n (_s: state) _res = false (* unused in stress_test_par mode *)
 end
 
-module GC_STM_dom = STM_domain.Make(Spec)
+let rec gen_cmds arb s fuel =
+  QCheck.Gen.(if fuel = 0
+       then return []
+       else
+         (arb s).QCheck.gen >>= fun c ->
+         let s' = try Spec.next_state c s with _ -> s in
+         (gen_cmds arb s' (fuel-1)) >>= fun cs ->
+         return (c::cs))
+
+let gen_cmds_size gen s size_gen = QCheck.Gen.sized_size size_gen (gen_cmds gen s)
+
+let arb_triple seq_len par_len arb0 arb1 arb2 =
+  let seq_pref_gen = gen_cmds_size arb0 Spec.init_state (QCheck.Gen.int_bound seq_len) in
+(*let shrink_triple = shrink_triple arb0 arb1 arb2 in*)
+  let gen_triple =
+    QCheck.Gen.(seq_pref_gen >>= fun seq_pref ->
+         int_range 2 (2*par_len) >>= fun dbl_plen ->
+         let spawn_state = List.fold_left (fun st c -> try Spec.next_state c st with _ -> st) Spec.init_state seq_pref in
+         let par_len1 = dbl_plen/2 in
+         let par_gen1 = gen_cmds_size arb1 spawn_state (return par_len1) in
+         let par_gen2 = gen_cmds_size arb2 spawn_state (return (dbl_plen - par_len1)) in
+         triple (return seq_pref) par_gen1 par_gen2) in
+  QCheck.make ~print:(Util.print_triple_vertical Spec.show_cmd) (*~shrink:shrink_triple*) gen_triple
+
+let arb_cmds_triple seq_len par_len = arb_triple seq_len par_len Spec.arb_cmd Spec.arb_cmd Spec.arb_cmd
+
+let arb_cmds_triple = arb_cmds_triple
+
+
+let interp_sut_res sut cs =
+  let cs_arr = Array.of_list cs in
+  let res_arr = Array.map (fun c -> Domain.cpu_relax(); Spec.run c sut) cs_arr in
+  List.combine cs (Array.to_list res_arr)
+
+let run_par seq_pref cmds1 cmds2 =
+  let sut = Spec.init_sut () in
+  let pref_obs = interp_sut_res sut seq_pref in
+  let barrier = Atomic.make 2 in
+  let main cmds () =
+    Atomic.decr barrier;
+    while Atomic.get barrier <> 0 do Domain.cpu_relax() done;
+    try Ok (interp_sut_res sut cmds) with exn -> Error exn
+  in
+  let dom1 = Domain.spawn (main cmds1) in
+  let dom2 = Domain.spawn (main cmds2) in
+  let obs1 = Domain.join dom1 in
+  let obs2 = Domain.join dom2 in
+  let ()   = Spec.cleanup sut in
+  let obs1 = match obs1 with Ok v -> v | Error exn -> raise exn in
+  let obs2 = match obs2 with Ok v -> v | Error exn -> raise exn in
+  pref_obs, obs1, obs2
+
+let stress_prop_par (seq_pref,cmds1,cmds2) =
+  let _ = run_par seq_pref cmds1 cmds2 in
+  true
+
+(* Common magic constants *)
+let rep_count = 25 (* No. of repetitions of the non-deterministic property *)
+let retries = 10   (* Additional factor of repetition during shrinking *)
+let seq_len = 20   (* max length of the sequential prefix *)
+let par_len = 12   (* max length of the parallel cmd lists *)
+
+let stress_test_par ~count ~name =
+  let max_gen = 3*count in (* precond filtering may require extra generation: max. 3*count though *)
+  QCheck.Test.make ~retries ~max_gen ~count ~name
+    (arb_cmds_triple seq_len par_len)
+    (fun triple ->
+       Printf.printf ".%!";
+       Util.repeat rep_count stress_prop_par triple) (* 25 times each, then 25 * 10 times when shrinking *)
 
 let _ =
   QCheck.Test.check_exn (
-    GC_STM_dom.stress_test_par    ~count:2000 ~name:"STM Gc stress test parallel";
+    stress_test_par ~count:2000 ~name:"STM Gc stress test parallel";
   )
