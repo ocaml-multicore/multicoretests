856fc74a5fa2a309e404926f664b67b5fbe4f5a5
Jan Midtgaard <mail@janmidtgaard.dk>
2025-05-28 14:34:47 +0200

    Remove filename concatenation

--- src/sys/stm_tests.ml
+++ src/sys/stm_tests.ml
@@ -14,8 +14,6 @@ let show_cmd x =
   | Rmdir (x, y) -> Printf.sprintf "Rmdir (%s, %s)" (show_path x) y
   | Mkfile (x, y) -> Printf.sprintf "Mkfile (%s, %s)" (show_path x) y
 
-let (/) = Filename.concat
-
 let sandbox_root = "_sandbox"
 
 let init_sut () =
@@ -28,8 +26,6 @@ let cleanup _ =
   | "Unix"  -> ignore (Sys.command ("rm -r " ^ Filename.quote sandbox_root))
   | "Win32" -> ignore (Sys.command ("rd /s /q " ^ Filename.quote sandbox_root))
   | v -> failwith ("Sys tests not working with " ^ v)
-
-let p path =  (List.fold_left (/) sandbox_root path)
 
 let mkfile filepath =
   let flags = [Open_wronly; Open_creat; Open_excl] in
@@ -58,31 +54,31 @@ let triple =
 let stress_prop_par () =
   let sut = init_sut () in
 
-  protect (fun () -> Sys.mkdir (p ["hhh"]) 0o755) () |> ignore;
-  protect (fun () -> mkfile (p ["hhh"; "iii"])) () |> ignore;
-  protect (fun () -> Sys.mkdir (p ["hhh"; "hhh"]) 0o755) () |> ignore;
+  protect (fun () -> Sys.mkdir "_sandbox/hhh" 0o755) () |> ignore; (* file exists? *)
+  protect (fun () -> mkfile "_sandbox/hhh/iii") () |> ignore;
+  protect (fun () -> Sys.mkdir "_sandbox/hhh/hhh" 0o755) () |> ignore;
 
   let barrier = Atomic.make 2 in
   let dom1 () =
     Atomic.decr barrier;
     while Atomic.get barrier <> 0 do Domain.cpu_relax() done;
     Domain.cpu_relax();
-    protect (fun () -> Sys.mkdir (p ["hhh"; "iii"; "eee"]) 0o755) () |> ignore;
+    protect (fun () -> Sys.mkdir "_sandbox/hhh/iii/eee" 0o755) () |> ignore;
     Domain.cpu_relax();
-    protect (fun () -> Sys.rename (p ["hhh"; "hhh"]) (p [])) () |> ignore;
+    protect (fun () -> Sys.rename "_sandbox/hhh/hhh" "_sandbox") () |> ignore;
     Domain.cpu_relax();
-    protect (fun () -> Sys.rename (p ["bbb"]) (p [])) () |> ignore;
+    protect (fun () -> Sys.rename "_sandbox/bbb" "_sandbox") () |> ignore;
   in
   let dom2 () =
     Atomic.decr barrier;
     while Atomic.get barrier <> 0 do Domain.cpu_relax() done;
-    protect (fun () -> Sys.rename (p ["hhh"; "iii"]) (p ["iii"; "ccc"])) () |> ignore;
+    protect (fun () -> Sys.rename "_sandbox/hhh/iii" "_sandbox/iii/ccc") () |> ignore;
     Domain.cpu_relax();
-    protect (fun () -> Sys.rmdir (p ["hhh"])) () |> ignore;
+    protect (fun () -> Sys.rmdir "_sandbox/hhh") () |> ignore;
     Domain.cpu_relax();
-    protect (fun () -> Sys.mkdir (p ["hhh"; "iii"]) 0o755) () |> ignore;
+    protect (fun () -> Sys.mkdir "_sandbox/hhh/iii" 0o755) () |> ignore;
     Domain.cpu_relax();
-    protect (fun () -> Sys.rmdir (p ["hhh"])) () |> ignore;
+    protect (fun () -> Sys.rmdir "_sandbox/hhh") () |> ignore;
   in
   let dom1 = Domain.spawn dom1 in
   let dom2 = Domain.spawn dom2 in
